bf9f1f162a0b9fda7575f29d1fad3084e5943401
Martin Norling <norlingjr@gmail.com>
2020-04-15 13:22:17 +0200

    Add example api endpoints

--- app/herdbook.py
+++ app/herdbook.py
@@ -6,11 +6,50 @@ The server uses Flask to serve a React frontend, connect to a postgres database,
 and do calculations with R.
 """
 
-from flask import Flask, render_template
+import db
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__,
             template_folder="/templates",
             static_folder="/static")
+
+@app.route('/api/herds')
+def herds():
+    herds = []
+    for herd in db.Herd.select():
+        herds += [{'id': herd.id,
+                   'name': herd.name,
+                   'individuals': herd.individual_set.count()
+                   }]
+    return jsonify(herds=herds)
+
+@app.route('/api/individual/<int:individual_id>')
+def individual(individual_id):
+    individual = db.Individual.get(individual_id)
+    return jsonify(individual={
+        'id': individual.id,
+        'herd': individual.herd.id,
+        'name': individual.name,
+        'certificate': individual.certificate,
+        'number': individual.number,
+        'sex': individual.sex,
+        'birth_date': individual.birth_date,
+        'mother': {
+            'id': individual.mother.id,
+            'name': individual.mother.name
+            },
+        'father': {
+            'id': individual.father.id,
+            'name': individual.father.name
+            },
+        'colour': individual.colour.name,
+        'colour_note': individual.colour_note,
+        'death_date': individual.death_date,
+        'death_note': individual.death_note,
+        'weight_young': individual.weight_young,
+        'litter': individual.litter,
+        'notes': individual.notes
+    })
 
 @app.route('/')
 def main():
