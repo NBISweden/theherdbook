a7d30cd698d4897e21a79faa6cc7abf2209fa8d4
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2020-05-12 12:44:03 +0200

    Spelling, quoting, stuff

--- init_db.sh
+++ init_db.sh
@@ -1,34 +1,36 @@
 #
 # This script reads the configuration file in app/config.ini to access a
-# postgres database and chreate the tables for the herdbook schema.
+# Postgres database and create the tables for the herdbook schema.
 #
-# Note that this script only needs to be run if you wish to create the database
-# without using the herdbook application, as the app will create the database
-# on startup.
+# Note that this script only needs to be run if you wish to initialize
+# the database without using the herdbook application, as the app will
+# initialize the database on startup.
 #
 
-cd "$(dirname $0)"
+cd "$( dirname "$0" )" || { echo 'cd error' >&2; exit 1; }
 
-if [ ! -e "app/config.ini" ]
+if [ ! -e app/config.ini ]
 then
-    echo "Couldn't find config file 'app/config.ini'"
-    echo "Create this file from 'app/config.ini.default' with the credentials"
-    echo "to your database and then run this script again."
+    cat <<-'END_ERROR' >&2
+	Couldn't find config file 'app/config.ini'
+	Create this file from 'app/config.ini.default' with the credentials
+	to your database and then run this script again.
+	END_ERROR
     exit 1
 fi
 
-if [ ! -d "venv" ]
+if [ ! -d venv ]
 then
-    printf "Creating python3 virtual environment in ./venv   "
+    printf 'Creating python3 virtual environment in ./venv   '
     python3 -m venv venv >/dev/null
-    echo "DONE"
-    printf "Installing app dependencies                      "
+    echo DONE
+    printf 'Installing app dependencies                      '
     pip3 install -r app/requirements.txt >/dev/null
-    echo "DONE"
+    echo DONE
 fi
 
-printf "Initializing database                            "
+printf 'Initializing database                            '
 . venv/bin/activate
 cd app/
-python3 -c "import utils.database as db; db.init()"
-echo "DONE"
+python3 -c 'import utils.database as db; db.init()'
+echo DONE
