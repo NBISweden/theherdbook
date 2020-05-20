64d254979e7d631e9c48f0ac215595753752ba89
Martin Norling <norlingjr@gmail.com>
2020-05-20 12:23:57 +0200

    fix: typo

--- app/herdbook.py
+++ app/herdbook.py
@@ -22,7 +22,7 @@ APP = Flask(__name__, static_folder="/static")
 APP.secret_key = uuid.uuid4().hex
 # cookie options at https://flask.palletsprojects.com/en/1.1.x/security/
 APP.config.update(
-    SESSION_COOKIR_SECURE = False if APP.config['ENV'] == 'development' else True,
+    SESSION_COOKIE_SECURE = False if APP.config['ENV'] == 'development' else True,
     SESSION_COOKIE_HTTPONLY = True,
     SESSION_COOKIE_SAMESITE = 'Lax' if APP.config['ENV'] == 'development' else 'Strict',
 )
