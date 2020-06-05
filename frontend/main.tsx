f75c8680d47ac9ff6778685314cb6210d59eccee
Martin Norling <norlingjr@gmail.com>
2020-06-05 10:33:27 +0200

    Add main div to content

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import styled, * as sc from 'styled-components'
 import CssBaseline from '@material-ui/core/CssBaseline';
+import {makeStyles} from '@material-ui/core/styles';
 import {
   BrowserRouter,
   Switch,
@@ -24,6 +25,12 @@ const CSS = sc.createGlobalStyle`
     margin: 1cm;
   }
 `
+const useMainStyle = makeStyles({
+  main: {
+    padding: "1cm",
+    paddingTop: 0,
+  }
+});
 
 function Routed(props: {path: string, children: (params: Record<string, string>) => React.ReactNode}) {
   function Inner() {
@@ -40,6 +47,7 @@ function Routed(props: {path: string, children: (params: Record<string, string>)
 }
 
 function Main() {
+  const classes = useMainStyle();
   return <>
     <CssBaseline />
     <WithUserContext>
@@ -50,29 +58,31 @@ function Main() {
         {/* Declare routes, and what component should be rendered for each
           * route.
           */}
-        <Switch>
-          <Route path="/login">
-            <Login/>
-          </Route>
-          <Route path="/genebanks">
-            <Genebanks/>
-          </Route>
-          <Routed path="/genebank/:id">
-            {params => <Genebank id={params.id}/>}
-          </Routed>
-          <Routed path="/herd/:id">
-            {params => <Herd id={params.id}/>}
-          </Routed>
-          <Routed path="/individual/:id">
-            {params => <Individual id={params.id}/>}
-          </Routed>
-          <Route path="/manage">
-            <Manage/>
-          </Route>
-          <Route path="/">
-            Welcome!
-          </Route>
-        </Switch>
+        <div className={classes.main}>
+          <Switch>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route path="/genebanks">
+              <Genebanks/>
+            </Route>
+            <Routed path="/genebank/:id">
+              {params => <Genebank id={params.id}/>}
+            </Routed>
+            <Routed path="/herd/:id">
+              {params => <Herd id={params.id}/>}
+            </Routed>
+            <Routed path="/individual/:id">
+              {params => <Individual id={params.id}/>}
+            </Routed>
+            <Route path="/manage">
+              <Manage/>
+            </Route>
+            <Route path="/">
+              Welcome!
+            </Route>
+          </Switch>
+        </div>
       </BrowserRouter>
     </WithUserContext>
     <CSS/>
