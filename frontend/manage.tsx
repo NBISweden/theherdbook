ace42211a2886c9ddad62843c4a94e894481deb8
Martin Norling <norlingjr@gmail.com>
2020-06-05 12:39:11 +0200

    Add tab structure for manage page

--- frontend/manage.tsx
+++ frontend/manage.tsx
@@ -6,9 +6,60 @@
 import React from 'react'
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { useUserContext } from './user_context'
 import { get } from './communication';
+
+// Define styles for tab menu
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+  breadcrumbs: {
+    padding: "15px",
+    paddingBottom: 0,
+  }
+});
+
+/**
+ * Props for controlling a TabPanel. the `index` and `value` are compared to
+ * check if the panel should be visible (`index` == `value`) or hidden.
+ */
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+/**
+ * Creates a Box containing the children props of the component, which will be
+ * rendered when the `index` prop is equal to the `value` prop.
+ *
+ * @param props - A set of `TabPanelProps` used to set content and visibility
+ */
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
 
 /**
  * Provides genebanks management forms for granting and revoking herd
@@ -18,6 +69,8 @@ export function Manage() {
   const {user} = useUserContext();
   const [genebanks, setGenebanks] = React.useState([] as any[])
   const [genebank, setGenebank] = React.useState(undefined as any)
+  const [currentTab, setTab] = React.useState(0);
+  const classes = useStyles();
 
   function selectGenebank(id: number) {
     get(`/api/genebank/${id}`).then(
@@ -39,24 +92,37 @@ export function Manage() {
     );
   }, [user])
 
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setTab(newValue);
+  };
+
   return <>
-    <h2>Administrera</h2>
-    <Breadcrumbs separator="&bull;" aria-label="breadcrumb">
+    <Breadcrumbs className={classes.breadcrumbs} separator="&bull;" aria-label="breadcrumb">
       {
         genebanks.map((g:any, i:number) => {
                 return  <a key={i} onClick={() => selectGenebank(g.id)}>
-                          <Typography color="textPrimary">
+                          <Typography color={genebank && g.id == genebank.id ? 'primary' : 'textSecondary'}>
                             {g.name}
                           </Typography>
                         </a>
         })
       }
     </Breadcrumbs>
-    <hr />
-    {genebank &&
-      <>
-        Det är {genebank.herds.length} besättningar i genbanken {genebank.name}.
-      </>
-    }
+    <Paper className={classes.root}>
+      <Tabs value={currentTab}
+            onChange={handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+        >
+           <Tab label="Besättningar" />
+           <Tab label="Användare" />
+      </Tabs>
+      <TabPanel value={currentTab} index={0}>
+        {genebank.name} innehar {genebank.herds.length} besättningar.
+      </TabPanel>
+      <TabPanel value={currentTab} index={1}>
+        Användarkontroller kommer att hamna här.
+      </TabPanel>
+    </Paper>
   </>
 }
