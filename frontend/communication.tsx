257f498fa3b74012769c723c5deedd4de61c812b
Martin Norling <norlingjr@gmail.com>
2020-06-12 14:45:56 +0200

    Add controls for updating basic herd data

--- frontend/communication.tsx
+++ frontend/communication.tsx
@@ -49,3 +49,24 @@ export async function post(url: string, content: any) {
   );
   return await resp.json();
 }
+
+/**
+ * Creates an UPDATE request to the given `url` containing `content`, and
+ * returnsthe reply as json.
+ *
+ * @param url The target URL for the POST request
+ * @param content The content to be sent with the request
+ */
+export async function update(url: string, content: any) {
+  const resp = await fetch(backend_url + url, {
+    body: JSON.stringify(content),
+    method: 'UPDATE',
+    credentials: credentials_policy,
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  }
+  );
+  return await resp.json();
+}
