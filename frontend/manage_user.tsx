953e1191400ae55c7ed8279e8433a407c97ab31e
Martin Norling <norlingjr@gmail.com>
2020-06-16 08:01:26 +0200

    Basic user management form

--- frontend/manage_user.tsx
+++ frontend/manage_user.tsx
@@ -3,14 +3,98 @@
  * add users and set user permissions.
  */
 import React from 'react'
+import { TextField, Checkbox, FormControlLabel, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { get, update } from './communication';
+
+// Define styles for tab menu
+const useStyles = makeStyles({
+  form: {
+    borderLeft: "1px solid rgba(0,0,0,0.1)",
+    paddingLeft: "0.5cm",
+    display: "flex",
+    flexDirection: "column",
+  },
+  simpleField: {
+    width: "400px",
+  },
+});
+
+/** description of a user to manage */
+export interface ManagedUser {
+  id: number
+  email: string
+  validated: boolean
+  privileges: any
+}
 
 /**
  * Provides a form for changing user metadata and user roles.
  */
-export function ManageUser(props: {id: number | undefined}) {
+export function ManageUser({id}: {id: string}) {
+  const [user, setUser] = React.useState(undefined as ManagedUser | undefined)
+  const classes = useStyles();
+
+  React.useEffect(() => {
+    get(`/api/manage/user/${id}`).then(
+      data => data && setUser(data),
+      error => console.error(error)
+    );
+  }, [id])
+
+  const submitForm = () => {
+    let postData = Object.assign({}, user);
+    delete postData["privileges"];
+    update(`/api/manage/user/${id}`, postData).then(
+      data => {
+        switch (data.status) {
+          case "updated": console.debug("updated successfully"); break;
+          default: console.debug("status:", data)// "failed" or other erro
+        }
+      },
+      error => console.error(error)
+    )
+  }
 
   return <>
-    <h2>User {props.id}</h2>
+    {user && <>
+      <h2>{user.email}</h2>
+      <form className={classes.form} noValidate autoComplete="off">
+          <TextField label='E-mail'
+                     className={classes.simpleField}
+                     value={user.email ?? undefined}
+                     onChange={e => setUser({...user, email: e.target.value})}
+            />
+          <FormControlLabel
+            control={
+              <Checkbox color="primary"
+                        checked={user.validated ?? undefined}
+                        onChange={e => setUser({...user, validated: e.target.checked})}
+                />
+            }
+            label="Validated"
+            labelPlacement="end"
+          />
+      </form>
+      <Button variant="contained"
+              color="primary"
+              onClick={() => submitForm()}>
+        Spara
+      </Button>
+    </>
+    }
+    <h3>Roles</h3>
+    <ul>
+      {user && <>
+        {user.privileges.map((role: any, i: number) => {
+            return <li key={i}>
+              {role.level}
+              {role.genebank && `, Genebank: ${role.genebank}`}
+              {role.herd && `, herd: ${role.herd}`}
+            </li>
+          })
+        }
+      </>}
+    </ul>
   </>
 }
