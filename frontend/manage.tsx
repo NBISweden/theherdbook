c4a60a30512623afcff1333ab5cb0fa9e039be65
Dan Rosén <dan.rosen@nbis.se>
2020-06-22 18:12:05 +0200

    Rework all routing and tabbing

--- frontend/manage.tsx
+++ frontend/manage.tsx
@@ -10,6 +10,7 @@ import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import { makeStyles } from '@material-ui/core/styles';
+import * as Router from 'react-router-dom'
 import {
   BrowserRouter,
   Switch,
@@ -20,8 +21,8 @@ import {
 } from "react-router-dom";
 
 import { useDataContext } from './data_context'
-import { ManageHerds } from '~manage_herds'
-import { ManageUsers } from '~manage_users'
+import { ManageHerds } from './manage_herds'
+import { ManageUsers } from './manage_users'
 
 // Define styles for tab menu
 const useStyles = makeStyles({
@@ -51,83 +52,72 @@ interface TabPanelProps {
   className?: any;
 }
 
-/**
- * Provides genebanks management forms for granting and revoking herd
- * permissions, and managing herd animals.
- */
-export function Manage({id}: {id: number | undefined}) {
+import * as ui from './ui_utils'
+
+function InnerPaper(props: {id: number}) {
+  const classes = useStyles()
   const {genebanks} = useDataContext()
   const [genebank, setGenebank] = React.useState(undefined as any)
-  const [currentTab, setTab] = React.useState(0);
-  const classes = useStyles()
-  let history = useHistory()
-  let match = useRouteMatch()
 
-  function selectGenebank(id: number | undefined) {
-    if (id == undefined && genebanks.length > 0) {
-      history.push(`manage/${genebanks[0].id}`)
-    } else {
-      let data = genebanks.filter(g => g.id == id)
-      if (data.length > 0) {
-        setGenebank(data[0])
-      }
+  const id = props.id
+
+  function selectGenebank(id: number) {
+    const data = genebanks.find(g => g.id == id)
+    if (data) {
+      setGenebank(data)
     }
   }
 
   React.useEffect(() => {
     selectGenebank(id);
-  }, [genebanks, id])
+  }, [id])
 
-  React.useEffect(() => {
-    let subPath = undefined
-    if (location.pathname.search(match.url) == 0) {
-      subPath = location.pathname.replace(match.url, '')
-      if (subPath.search('/herds') == 0) {
-        setTab(0)
-      } else if (subPath.search('/users') == 0) {
-        setTab(1)
-      }
-    }
-  }, [history])
+  const tabs: ui.RoutedTab[] = [
+    {
+      path: '/herds',
+      label: 'Besättningar',
+      component: <ManageHerds id={id}/>,
+    },
+    {
+      path: '/users',
+      label: 'Användare',
+      component: <ManageUsers/>
+    },
+  ]
 
-  const tabChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-    setTab(newValue)
-  }
+  const {Tabs, TabbedRoutes} = ui.useRoutedTabs(tabs, {autoselect_first: true})
+
+  return (
+    <Paper className={classes.paper}>
+      <Tabs/>
+      <Switch>
+        {TabbedRoutes}
+      </Switch>
+    </Paper>
+  )
+}
+
+
+/**
+ * Provides genebanks management forms for granting and revoking herd
+ * permissions, and managing herd animals.
+ */
+export function Manage() {
+  const {genebanks} = useDataContext()
+
+  const tabs: ui.RoutedTab[] = genebanks.map(g => ({
+    path: `/${g.id}`,
+    label: g.name,
+    component: <InnerPaper id={g.id}/>,
+  }))
+
+  const {Tabs, TabbedRoutes} = ui.useRoutedTabs(tabs, {autoselect_first: true})
 
   return <>
-    <Breadcrumbs className={classes.breadcrumbs} separator="&bull;" aria-label="breadcrumb">
-      {
-        genebanks.map((g:any, i:number) => {
-          return  <Link key={i} to={`/manage/${g.id}`} className={classes.breadcrumb}>
-                    <Typography color={genebank && g.id == genebank.id ? 'primary' : 'textSecondary'}>
-                      {g.name}
-                    </Typography>
-                  </Link>
-        })
-      }
-    </Breadcrumbs>
-    <Paper className={classes.paper}>
-      <BrowserRouter>
-        <Tabs value={currentTab}
-              onChange={tabChange}
-              indicatorColor="primary"
-              textColor="primary"
-          >
-          <Link to={`${match.url}/herds`}><Tab label="Besättningar" /></Link>
-          <Link to={`${match.url}/users`}><Tab label="Användare" /></Link>
-        </Tabs>
-        <Switch>
-          <Route path={`${match.path}/herds`}>
-            <ManageHerds id={id}/>
-          </Route>
-          <Route path={`${match.path}/users`}>
-            <ManageUsers/>
-          </Route>
-          <Route path={match.path}>
-            <ManageHerds id={id}/>
-          </Route>
-        </Switch>
-      </BrowserRouter>
-    </Paper>
+    <Tabs/>
+    <Switch>
+      {TabbedRoutes}
+    </Switch>
   </>
 }
+
--- frontend/navigation.tsx
+++ frontend/navigation.tsx
@@ -2,86 +2,113 @@
 import * as React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
 import HomeIcon from '@material-ui/icons/Home';
 import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import VpnKeyIcon from '@material-ui/icons/VpnKey';
 import MeetingRoom from '@material-ui/icons/MeetingRoom';
 import GroupIcon from '@material-ui/icons/Group';
-import {useHistory} from 'react-router-dom'
-import {Location} from 'history'
+
+import {Login} from './login'
+import {Genebanks} from './genebanks'
+import {Genebank} from './genebank'
+import {Herd} from './herd'
+import {Manage} from './manage'
+import {Individual} from './individual'
+
+import {Switch, Route} from 'react-router-dom'
 
 import {useUserContext} from './user_context'
 
+import * as ui from './ui_utils'
+
 // Define styles for tab menu
-const useMenuStyles = makeStyles({
+const useStyles = makeStyles({
   root: {
     flexGrow: 1,
   },
+  main: {
+    height: "calc(100% - 72px)",
+  }
 });
 
-/**
- * Returns a menu with tabs hidden when they are not applicable to the current
- * user.
- */
-export function TabMenu() {
-  const classes = useMenuStyles();
-  const [value, setTab] = React.useState(0);
+export function Navigation() {
+
+  const classes = useStyles();
+
   const {logout} = useUserContext();
   const {user} = useUserContext();
-  const history = useHistory();
 
   const is_admin = !!(user?.is_manager || user?.is_admin)
   const is_logged_in = !!user
 
-  const tabs = [
-    {label: "Hem",          route: "/",          show: true,                            icon: <HomeIcon />           },
-    {label: "Genbanker",    route: "/genebanks", show: is_logged_in,                    icon: <AccountBalanceIcon /> },
-    {label: "Administrera", route: "/manage",    show: is_admin,                        icon: <GroupIcon />          },
-    {label: "Logga in",     route: "/login",     show: !is_logged_in,                   icon: <MeetingRoom />        },
-    {label: "Logga ut",     route: "/",          show: is_logged_in,  on_click: logout, icon: <VpnKeyIcon />         },
+  const tabs: ui.RoutedTab[] = [
+    {
+      label: "Hem",
+      path: "/",
+      exact: true,
+      component: 'Welcome!',
+      visible: true,
+      icon: <HomeIcon />
+    },
+    {
+      label: "Genbanker",
+      path: "/genebanks",
+      component: <Genebanks/>,
+      visible: is_logged_in,
+      icon: <AccountBalanceIcon />
+    },
+    {
+      label: "Administrera",
+      path: "/manage",
+      component: <Manage/>,
+      visible: is_admin,
+      icon: <GroupIcon />
+    },
+    {
+      label: "Logga in",
+      path: "/login",
+      component: <Login/>,
+      visible: !is_logged_in,
+      icon: <MeetingRoom />
+    },
+    {
+      label: "Logga ut",
+      path: "/",
+      exact: true,
+      visible: is_logged_in,
+      on_click: logout,
+      icon: <VpnKeyIcon />
+    },
   ]
 
-  const handleChange = (_: any, index: number) => {
-    const tab = tabs[index]
-    if (tab.on_click) {
-      tab.on_click()
-    }
-    if (tab.route) {
-      history.push(tab.route)
-    }
-  };
+  const {Tabs, TabbedRoutes} = ui.useRoutedTabs(tabs)
 
-  React.useEffect(() => {
-    const with_location = (location: Location) => {
-      const basePath = '/' + location.pathname.split('/')[1]
-      const index = tabs.findIndex(t => t.route == basePath)
-      if (index != -1) {
-        setTab(index)
-      } else {
-        console.warn('Route', basePath, 'has no corresponding tab')
-      }
-    }
-    with_location(history.location)
-    const unsubscribe = history.listen(with_location)
-    return unsubscribe
-  }, [])
-
-  return (
+  return <>
+    {/* Insert the tab menu */}
     <Paper className={classes.root}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        indicatorColor="primary"
-        textColor="primary"
-        centered
-      >
-        { tabs.map((tab: any, i: number) => {
-            return <Tab key={i} icon={tab.icon} label={tab.label} style={{display: tab.show ? undefined : 'none'}}/>
-          })
-        }
-      </Tabs>
+      <Tabs centered/>
     </Paper>
-  );
+
+    {/* Declare routes, and what component should be rendered for each
+      * route.
+      */}
+
+    <div className={classes.main}>
+      <Switch>
+        {TabbedRoutes}
+        <ui.Routed path="/genebank/:id">
+          {params => <Genebank id={params.id}/>}
+        </ui.Routed>
+        <ui.Routed path="/herd/:id">
+          {params => <Herd id={params.id}/>}
+        </ui.Routed>
+        <ui.Routed path="/individual/:id">
+          {params => <Individual id={params.id}/>}
+        </ui.Routed>
+        <Route path="/">
+          Welcome!
+        </Route>
+      </Switch>
+    </div>
+  </>
 }
