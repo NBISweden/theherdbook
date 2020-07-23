f0d57145e9f209d52180e229af2f827e448b01e9
Airen Zaldivar <airenzp@gmail.com>
2020-07-23 14:37:49 +0200

    removed not required import

--- frontend/family-tree/member.js
+++ frontend/family-tree/member.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { MdPerson } from "react-icons/md";
 import icon from './rabbit-icon.png';
 
 const StyledWrapper = styled.div`
