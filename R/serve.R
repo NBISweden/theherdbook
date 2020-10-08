ab23477877458f87b42ecf3f8200eb17a123343b
Jonas Hagberg <jonas.hagberg@nbis.se>
2020-10-08 14:07:41 +0200

    Env variable changes to R files

--- R/serve.R
+++ R/serve.R
@@ -1,10 +1,4 @@
 library(plumber)
 r <- plumber::plumb(here::here("R/api.R"))
-
-bind_addr <- Sys.getenv("OPTISEL_BIND_ADDR", "localhost") 
-port <- Sys.getenv("OPTISEL_PORT", "31113") 
-
-r$run(host = get("bind_addr"), port = as.integer(get("port")))
-
-
-
+port <- Sys.getenv("RAPI_PORT", "31113") 
+r$run( port = as.integer(get("port")), swagger=FALSE)
