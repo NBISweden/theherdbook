05bcfa4aaff15562e4637b7c72714be8e895f81f
jonandernovella <novella.uu@gmail.com>
2020-10-25 16:06:43 +0100

    Rename R api settings
    The name should follow the conventions.

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -9,7 +9,7 @@ from argparse import Namespace
 logging.info("Reading Environemt")
 
 postgres = Namespace()  # pylint: disable=C0103
-r-api = Namspace()
+rapi = Namespace() # pylint: disable=C0103
 #Read configuration from environment variable
 
 postgres.name = os.environ.get("POSTGRES_DB", "herdbook")
@@ -18,5 +18,5 @@ postgres.port = os.environ.get("POSTGRES_PORT", "5432")
 postgres.user = os.environ.get("POSTGRES_USER", "herdbook")
 postgres.password = os.environ.get("POSTGRES_PASSWORD", "insecure")
 
-r-api.host = os.environ.get("RAPI_HOST", "r-api")
-r-api.port = os.environ.get("RAPI_PORT", "31113")
+rapi.host = os.environ.get("RAPI_HOST", "r-api")
+rapi.port = os.environ.get("RAPI_PORT", "31113")
