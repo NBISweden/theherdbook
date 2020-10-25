abcc65034a037e2c320908221cc6e31ac0920423
jonandernovella <novella.uu@gmail.com>
2020-10-25 16:07:22 +0100

    Add csv parser utility
    This is used to parse the data from the R api.

---
+++ app/utils/csvparser.py
@@ -0,0 +1,29 @@
+"""
+Utility for parsing csv fetched from the R Api
+"""
+
+def parse_kinship(resp):
+    """
+    Parses a csv in order to return a dictionary with the kinship coefficients.
+    """
+    content_utf = resp.content.decode('utf-8')
+    csv_list = [[val.strip() for val in r.split(",")] for r in content_utf.splitlines()]
+    (header,*data) = csv_list
+    csv_dict = {}
+    for idx,row in enumerate(data):
+        csv_dict[header[idx]]={key: value for key, value in zip(header, map(float, row))}
+    return csv_dict
+
+
+def parse_csv(resp):
+    """
+    Parses a csv in order to return a dictionary.
+    """
+    content_utf = resp.content.decode('utf-8')
+    csv_list = [[val.strip() for val in r.split(",")] for r in content_utf.splitlines()]
+    (_,*data) = csv_list
+    csv_dict = {}
+    for row in data:
+        key, *values = row
+        csv_dict[key] = float(values[0])
+    return csv_dict
