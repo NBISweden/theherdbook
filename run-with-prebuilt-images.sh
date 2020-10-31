e396a1cbaa1d867e3e4838aaca2a6417479f8ac1
Jonas Hagberg <jonas.hagberg@nbis.se>
2020-10-31 08:18:55 +0100

    Export TAG
    Export the TAG var so it works on other bash versions

--- run-with-prebuilt-images.sh
+++ run-with-prebuilt-images.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-GITHUB_REF="$(git rev-parse --abbrev-ref HEAD)" TAG=${GITHUB_REF##*/} docker-compose -f dc-current-branch.yml up
+GITHUB_REF="$(git rev-parse --abbrev-ref HEAD)"
+export TAG=${GITHUB_REF##*/}
+docker-compose -f dc-current-branch.yml up
