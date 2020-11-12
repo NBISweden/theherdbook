31c30e444428d9c8bc4baa688be0f1941ed85bf4
Martin Norling <norlingjr@gmail.com>
2020-11-12 14:13:26 +0100

    Add Individual.castrated field

--- app/utils/database.py
+++ app/utils/database.py
@@ -359,6 +359,7 @@ class Individual(BaseModel):
     belly_color = CharField()
     hair_notes = CharField()
     is_active = BooleanField()
+    castrated = BooleanField(default=False)
 
     @property
     def current_herd(self):
