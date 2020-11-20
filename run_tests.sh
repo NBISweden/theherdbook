48bdd8b08ac74f0dd916e263ed4970d9ebd9c509
Martin Norling <norlingjr@gmail.com>
2020-11-20 08:11:06 +0100

    Add python unittest convenience script

---
+++ run_tests.sh
@@ -0,0 +1,19 @@
+#
+# Simple convenience test runner for the herd book.
+#
+
+VENV="venv"
+
+if [ ! -d "$VENV" ]
+then
+  echo "Error: Could not find virtual environment '$VENV'."
+  echo "Create the environment with:\n\n    python -m venv $VENV.\n"
+  exit 1
+fi
+
+source "$VENV/bin/activate"
+
+cd app
+
+echo "Running python unittests"
+coverage run --omit "../$VENV/*" -m unittest test.py && coverage report
