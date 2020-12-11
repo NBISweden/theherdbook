375175e7e30fcd1169a249f1a3039a9a60ca868d
Pontus Freyhult <pontus_github@soua.net>
2020-12-11 14:06:46 +0100

    Formatted with prettier default

--- frontend/src/login.tsx
+++ frontend/src/login.tsx
@@ -1,18 +1,24 @@
-import React, {useState} from 'react'
-import {useHistory, useLocation} from 'react-router-dom'
+import React, { useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
-import { CircularProgress, makeStyles, Button, Container, TextField } from '@material-ui/core';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import {
+  CircularProgress,
+  makeStyles,
+  Button,
+  Container,
+  TextField,
+} from "@material-ui/core";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
-import { useMessageContext } from '@app/message_context';
+import { useMessageContext } from "@app/message_context";
 
-import {post, get} from './communication'
-import {useUserContext} from '@app/user_context'
-import { inputVariant } from '@app/data_context_global';
+import { post, get } from "@app/communication";
+import { useUserContext } from "@app/user_context";
+import { inputVariant } from "@app/data_context_global";
 
 const useStyles = makeStyles({
   loading: {
@@ -20,7 +26,7 @@ const useStyles = makeStyles({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-  }
+  },
 });
 
 /**
@@ -28,181 +34,183 @@ const useStyles = makeStyles({
  */
 export function Login() {
   const [open, setOpen] = React.useState(true);
-  const {user, login} = useUserContext()
-  const [username, set_username] = useState('')
-  const [password, set_password] = useState('')
-  const [authenticators, setAuthenticators] = useState([])
-  const {userMessage} = useMessageContext()
+  const { user, login } = useUserContext();
+  const [username, set_username] = useState("");
+  const [password, set_password] = useState("");
+  const [authenticators, setAuthenticators] = useState([]);
+  const { userMessage } = useMessageContext();
 
-  const styles = useStyles()
-  const history = useHistory()
+  const styles = useStyles();
+  const history = useHistory();
   const location = useLocation();
   const { from } = location.state || { from: { pathname: "/" } };
 
   const close = () => {
-    setOpen(false)
-    history.push("/")
-  }
+    setOpen(false);
+    history.push("/");
+  };
 
   const submitLogin = () => {
-    login(username, password)
-  }
+    login(username, password);
+  };
 
   React.useEffect(() => {
-       if (user) {
-      history.replace(from)
+    if (user) {
+      history.replace(from);
     }
-  }, [user])
-  
+  }, [user]);
+
   React.useEffect(() => {
-    getAvailableAuthenticators()
-  }, [])
+    getAvailableAuthenticators();
+  }, []);
 
   const keydown = (e: React.KeyboardEvent<any>) => {
     // Cannot get TextField to trigger onSubmit so listen for keydown instead
-    if (e.key == 'Enter') {
-      submitLogin()
+    if (e.key == "Enter") {
+      submitLogin();
     }
-  }
+  };
 
   if (user != null) {
-      return <div className={styles.loading}>
+    return (
+      <div className={styles.loading}>
         <CircularProgress />
       </div>
+    );
   }
 
   function waitForExternalLogin(w) {
-  // Wait for an external login window
+    // Wait for an external login window
     try {
-      if (w.document.readyState == "complete") { 
-    
-        var returnedUser = w.document.body.innerText.trim()
+      if (w.document.readyState == "complete") {
+        var returnedUser = w.document.body.innerText.trim();
         if (returnedUser == "null") {
-          userMessage("Identity not linked to any known user", "error") 
+          userMessage("Identity not linked to any known user", "error");
         } else {
-          userMessage("Logged in as "+returnedUser, "success")
-          document.location = '/'
+          userMessage("Logged in as " + returnedUser, "success");
+          document.location = "/";
         }
 
-        w.close()
-        return
+        w.close();
+        return;
       }
-    }
-    catch (err) {
-    } 
-    setTimeout( () => waitForExternalLogin(w), 100)
+    } catch (err) {}
+    setTimeout(() => waitForExternalLogin(w), 100);
   }
-  
+
   function authenticateExternal(service) {
-   // We are not doing account linking
+    // We are not doing account linking
     fetch("/api/link/reset", {
       body: "",
-      method: 'POST',
-      credentials: 'same-origin',
-      redirect: 'manual',
+      method: "POST",
+      credentials: "same-origin",
+      redirect: "manual",
       headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-    }
-    ).then( 
-      x => {
-   // Authenticate with external service service
-        
-      return fetch("/api/login/"+service, {
-      body: "",
-      method: 'POST',
-      credentials: 'same-origin',
-      redirect: 'manual',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-    }
-    )}).then(
-      response => {
-        if (response.ok) {
-          response.json().then(
-            data => { if (data == null) { 
-              userMessage("Identity not linked to any known user", "error") 
-            }          else {
-              userMessage("Logged in as "+data['username'], "success")
-              document.location = '/'
-            }}
-            ) }
-          else {
-            var w = window.open('/api/login/'+service, "_blank")
-            setTimeout( () => waitForExternalLogin(w), 20)
-          }}
-      ,
-      err => {
-        userMessage("Unexpected error", "error")
-      }
-    )
-  }
+    })
+      .then((x) => {
+        // Authenticate with external service service
 
-   async function getAvailableAuthenticators(l) {
-    await post('/api/login/available').then(
-      data => { 
-        if (!data) {
-          data = []
-        }
-        setAuthenticators(data)
-      },
-      error => {
+        return fetch("/api/login/" + service, {
+          body: "",
+          method: "POST",
+          credentials: "same-origin",
+          redirect: "manual",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
       })
+      .then(
+        (response) => {
+          if (response.ok) {
+            response.json().then((data) => {
+              if (data == null) {
+                userMessage("Identity not linked to any known user", "error");
+              } else {
+                userMessage("Logged in as " + data["username"], "success");
+                document.location = "/";
+              }
+            });
+          } else {
+            var w = window.open("/api/login/" + service, "_blank");
+            setTimeout(() => waitForExternalLogin(w), 20);
+          }
+        },
+        (err) => {
+          userMessage("Unexpected error", "error");
+        }
+      );
   }
 
+  async function getAvailableAuthenticators(l) {
+    await post("/api/login/available").then(
+      (data) => {
+        if (!data) {
+          data = [];
+        }
+        setAuthenticators(data);
+      },
+      (error) => {}
+    );
+  }
 
-        return <Dialog open={open} onClose={close} aria-labelledby="form-dialog-title">
-        <DialogTitle>Logga in</DialogTitle>
-        <form onSubmit={submitLogin} onKeyDown={keydown}>
-          <DialogContent>
-            <DialogContentText>
-              Logga in med ditt användarnamn eller e-postadress och ditt lösenord.
-            </DialogContentText>
-            <TextField
-              id="username"
-              variant={inputVariant}
-              autoFocus
-              margin="dense"
-              label="Användarnamn eller E-postadress"
-              type="email"
-              value={username}
-              onChange={e => set_username(e.target.value)}
-              onSubmit={submitLogin}
-              fullWidth
-            />
-            <TextField
-              id="password"
-              variant={inputVariant}
-              margin="dense"
-              label="Lösenord"
-              type="password"
-              value={password}
-              onChange={e => set_password(e.target.value)}
-              fullWidth
-            />
-
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={close} color="primary">
-              Avbryt
-            </Button>
-            <Button onClick={submitLogin} color="primary" aria-label="Logga in">
-              Logga in
-            </Button>
-            </DialogActions>
-            <DialogActions>
-            <Container>              
-            {                
-              authenticators.map((item) => (
-                <Button onClick={ () => authenticateExternal(item) } key={item} name={item}>
+  return (
+    <Dialog open={open} onClose={close} aria-labelledby="form-dialog-title">
+      <DialogTitle>Logga in</DialogTitle>
+      <form onSubmit={submitLogin} onKeyDown={keydown}>
+        <DialogContent>
+          <DialogContentText>
+            Logga in med ditt användarnamn eller e-postadress och ditt lösenord.
+          </DialogContentText>
+          <TextField
+            id="username"
+            variant={inputVariant}
+            autoFocus
+            margin="dense"
+            label="Användarnamn eller E-postadress"
+            type="email"
+            value={username}
+            onChange={(e) => set_username(e.target.value)}
+            onSubmit={submitLogin}
+            fullWidth
+          />
+          <TextField
+            id="password"
+            variant={inputVariant}
+            margin="dense"
+            label="Lösenord"
+            type="password"
+            value={password}
+            onChange={(e) => set_password(e.target.value)}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={close} color="primary">
+            Avbryt
+          </Button>
+          <Button onClick={submitLogin} color="primary" aria-label="Logga in">
+            Logga in
+          </Button>
+        </DialogActions>
+        <DialogActions>
+          <Container>
+            {authenticators.map((item) => (
+              <Button
+                onClick={() => authenticateExternal(item)}
+                key={item}
+                name={item}
+              >
                 {item}
-                </Button>
-            ))
-          }</Container>
-          </DialogActions>
-        </form>
-      </Dialog>
-     }
+              </Button>
+            ))}
+          </Container>
+        </DialogActions>
+      </form>
+    </Dialog>
+  );
+}
