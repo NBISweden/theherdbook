1429b9dbf119353c8d30b12f5578e36cb32298c5
Ingrid Hyltander <ingrid.hyltander@nbis.se>
2021-03-11 16:13:36 +0100

    Added ability to choose grandparents to breed instead of mother/father, semiugly but working draft

--- frontend/src/inbreeding_form.tsx
+++ frontend/src/inbreeding_form.tsx
@@ -8,7 +8,7 @@ import { Autocomplete } from '@material-ui/lab'
 import { createFilterOptions } from '@material-ui/lab/Autocomplete'
 import { TextField, Button, Typography, Paper } from '@material-ui/core'
 
-import { individualLabel, inputVariant, Individual, Genebank } from '@app/data_context_global'
+import { individualLabel, inputVariant, Individual, LimitedIndividual, Genebank } from '@app/data_context_global'
 import { useDataContext } from './data_context'
 import { useMessageContext } from '@app/message_context'
 import { InbreedingRecommendation } from '@app/inbreeding_recommendation'
@@ -23,15 +23,35 @@ const useStyles = makeStyles({
     paddingBottom: '120px'
   },
   formBox: {
-    padding: '10px',
+    padding: '2px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  input: {
+    padding: '3px',
+    paddingBottom: '5px',
+    margin: '10px',
+    flexBasis: '22%',
+    /*border: '3px solid darkblue',
+    borderRadius: '5px',
+    textAlign: 'center'*/
+    /*display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center'*/
+  },
+  inputGrandParents: {
+    margin: '5px',
+    flexBasis: '22%',
   },
   wideControl: {
-    width: '25%',
   },
   bottomButton: {
     float: 'left',
     marginTop: '20px',
-  }
+  },
+  testInput: {
+    paddingLeft: '15px'
+  },
 });
 
 /**
@@ -59,14 +79,24 @@ export function InbreedingForm() {
   const style = useStyles()
   const [female, setFemale] = React.useState(undefined as Individual | undefined)
   const [male, setMale] = React.useState(undefined as Individual | undefined)
+  const [grandMomFem, setGrandMomFem] = React.useState(undefined as LimitedIndividual | undefined)
+  const [grandDadFem, setGrandDadFem] = React.useState(undefined as LimitedIndividual | undefined)
+  const [grandMomMale, setGrandMomMale] = React.useState(undefined as LimitedIndividual | undefined)
+  const [grandDadMale, setGrandDadMale] = React.useState(undefined as LimitedIndividual | undefined)
   
 
   /* TODO, add ability to choose genebank when several are present in DB. Copy or rewrite genebanks? */
   const genebank = genebanks[0]
 
   const activeFemales : Individual[] = activeIndividuals(genebank, 'female')
+  const activeFemalesLimited : LimitedIndividual[] = activeFemales.map(i => {
+    return {id: i.id, name: i.name, number: i.number}
+  })
 
   const activeMales : Individual[] = activeIndividuals(genebank, 'male')
+  const activeMalesLimited : LimitedIndividual[] = activeMales.map(i => {
+    return {id: i.id, name: i.name, number: i.number}
+  })
 
   const filterOptions = createFilterOptions<Individual>({
   limit: 30,
@@ -77,49 +107,127 @@ export function InbreedingForm() {
   
   return <>
           <Paper>
-          <form className={style.form}>
-          <Typography variant='h6'>Provparning</Typography>
-          <div className={style.formBox}>
-          <Autocomplete
-                  options={activeFemales}
-                  getOptionLabel={(option: Individual) => individualLabel(option)}
-                  value={female}
-                  // FEEDBACK, not sure if onChange "typing" solution optimal
-                  onChange={(event, newValue) => {
-                    setFemale(newValue ? newValue : undefined);
-                  }}
-                  filterOptions={filterOptions}
-                  renderInput={(params) => <TextField {...params}
-                    label="Välj mor"
-                    className={style.wideControl}
-                    variant={inputVariant} />}
-          />
-          <Autocomplete
-                  options={activeMales}
-                  getOptionLabel={(option: Individual) => individualLabel(option)}
-                  value={male}
-                  // FEEDBACK, not sure if onChange "typing" solution optimal
-                  onChange={(event, newValue) => {
-                    setMale(newValue ? newValue : undefined);
-                  }}
-                  filterOptions={filterOptions}
-                  renderInput={(params) => <TextField {...params}
-                    label="Välj far"
-                    className={style.wideControl}
-                    variant={inputVariant} />}
-          />
-
-          <Button className={style.bottomButton}
-                         variant='contained'
-                         color='primary'
-                         disabled={!female || !male}
-                         onClick={() => popup(<InbreedingRecommendation female={female} male={male} coefficientOfInbreeding={coefficientOfInbreeding} sufficientGenerations={true}/>, undefined)}
-            >
-                  Beräkna inavelkoefficient
-          </Button>
-
-    </div>
-    </form>
-    </Paper>
-    </>
+            <form className={style.form}>
+              <Typography variant='h6'>Provparning</Typography>
+              <div className={style.formBox}>
+                <Autocomplete className={style.input}
+                        style={{ marginRight: '180px' }}
+                        options={activeFemales}
+                        getOptionLabel={(option: Individual) => individualLabel(option)}
+                        value={female}
+                        // FEEDBACK, not sure if onChange "typing" solution optimal
+                        onChange={(event, newValue) => {
+                          setFemale(newValue ? newValue : undefined)
+                          setGrandMomFem(newValue?.mother ? newValue?.mother : undefined)
+                          setGrandDadFem(newValue?.father ? newValue?.father : undefined)
+                        }}
+                        filterOptions={filterOptions}
+                        renderInput={(params) => <TextField {...params}
+                          label="Välj mor"
+                          helperText="Om ännu ej registrerad, välj morföräldrar nedan"
+                          className={style.wideControl}
+                          variant={"outlined"}
+                          />}
+                />
+                <Autocomplete className={style.input}
+                        style={{ marginLeft: '180px' }}
+                        options={activeMales}
+                        getOptionLabel={(option: Individual) => individualLabel(option)}
+                        value={male}
+                        // FEEDBACK, not sure if onChange "typing" solution optimal
+                        onChange={(event, newValue) => {
+                          setMale(newValue ? newValue : undefined)
+                          setGrandMomMale(newValue?.mother ? newValue.mother : undefined)
+                          setGrandDadMale(newValue?.father ? newValue.father : undefined)
+                        }}
+                        filterOptions={filterOptions}
+                        renderInput={(params) => <TextField {...params}
+                          placeholder="Test"
+                          label="Välj far"
+                          className={style.wideControl}
+                          variant={inputVariant} />}
+                />
+              </div>
+              <div className={style.formBox}>
+                <Autocomplete className={style.inputGrandParents}
+                        disabled={female ? true : false}
+                        options={activeFemalesLimited}
+                        getOptionLabel={(option: LimitedIndividual) => individualLabel(option)}
+                        getOptionSelected={(option, value) => option.id === value.id}
+                        value={grandMomFem}
+                        inputValue={grandMomFem ? individualLabel(grandMomFem) : ''}
+                        onChange={(event, newValue) => {
+                          setGrandMomFem(newValue ? newValue : undefined);
+                        }}
+                        filterOptions={filterOptions}
+                        renderInput={(params) => <TextField {...params}
+                          label="Välj mormor"
+                          className={style.wideControl}
+                          variant={"outlined"}
+                          />}
+                />
+                <Autocomplete className={style.inputGrandParents}
+                        style={{ marginRight: '15px' }}
+                        disabled={female ? true : false}
+                        getOptionSelected={(option, value) => option.id === value.id}
+                        options={activeMalesLimited}
+                        getOptionLabel={(option: LimitedIndividual) => individualLabel(option)}
+                        value={grandDadFem}
+                        inputValue={grandDadFem ? individualLabel(grandDadFem) : ''}
+                        onChange={(event, newValue) => {
+                          setGrandDadFem(newValue ? newValue : undefined);
+                        }}
+                        filterOptions={filterOptions}
+                        renderInput={(params) => <TextField {...params}
+                          label="Välj morfar"
+                          className={style.wideControl}
+                          variant={"outlined"} />}
+                />
+                <Autocomplete className={style.inputGrandParents}
+                        style={{ marginLeft: '15px' }}
+                        disabled={male ? true : false}
+                        options={activeFemalesLimited}
+                        getOptionLabel={(option: LimitedIndividual) => individualLabel(option)}
+                        getOptionSelected={(option, value) => option.id === value.id}
+                        value={grandMomMale}
+                        inputValue={grandMomMale ? individualLabel(grandMomMale) : ''}
+                        onChange={(event, newValue) => {
+                          setGrandMomMale(newValue ? newValue : undefined)
+                        }}
+                        filterOptions={filterOptions}
+                        renderInput={(params) => <TextField {...params}
+                          label="Välj farmor"
+                          className={style.wideControl}
+                          variant={"outlined"}
+                          />}
+                />
+                <Autocomplete className={style.inputGrandParents}
+                        disabled={male ? true : false}
+                        options={activeMalesLimited}
+                        getOptionLabel={(option: LimitedIndividual) => individualLabel(option)}
+                        getOptionSelected={(option, value) => option.id === value.id}
+                        value={grandDadMale}
+                        inputValue={grandDadMale ? individualLabel(grandDadMale) : ''}
+                        onChange={(event, newValue) => {
+                          setGrandDadMale(newValue ? newValue : undefined);
+                        }}
+                        filterOptions={filterOptions}
+                        renderInput={(params) => <TextField {...params}
+                          placeholder="Test"
+                          label="Välj farfar"
+                          className={style.wideControl}
+                          variant={"outlined"} />}
+                />
+              </div>
+              <Button className={style.bottomButton}
+                            variant='contained'
+                            color='primary'
+                            disabled={!female || !male}
+                            onClick={() => popup(<InbreedingRecommendation female={female} male={male} coefficientOfInbreeding={coefficientOfInbreeding} sufficientGenerations={true}/>, undefined)}
+                >
+                      Beräkna inavelkoefficient
+              </Button>  
+            </form>
+          </Paper>
+      </>
 }
