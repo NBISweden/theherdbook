6e32304b76664caa4b40d8dfc78d9cdec373f35b
Andreas (Kusalananda) Kähäri <andreas.kahari@nbis.se>
2021-03-11 13:55:30 +0100

    Remove unneeded code, simplify (?) stuff and prettify "Done" message at end

--- scripts/load-docker-compose.sh
+++ scripts/load-docker-compose.sh
@@ -13,67 +13,9 @@
 #	          -M herd-registry-mellerud.xlsx
 #
 
-usage () {
-	cat <<-USAGE_END
-	Usage:
-	    $0	\\
-	    	-g kanindata-gotland.xlsx -G herd-registry-gotland.xlsx \\
-	    	-m kanindata-mellerud.xlsx
-
-	Options:
-
-	    -g file	Load Gotland rabbit data from "file"
-	    -G file	Load Gotland herd data from "file"
-	    -m file	Load Mellerud rabbit data from "file"
-
-	    -h		Show this help text
-	USAGE_END
-}
-
-
-while getopts 'g:G:hm:M:' opt; do
-	case $opt in
-		g)	gfile=$OPTARG ;;
-		G)	Gfile=$OPTARG ;;
-		h)	usage; exit ;;
-		m)	mfile=$OPTARG ;;
-		M)	Mfile=$OPTARG ;;
-		*)
-			echo 'Error in command line parsing' >&2
-			usage >&2
-			exit 1
-	esac
-done
-shift "$(( OPTIND - 1 ))"
-
-load_gotland=true
-load_mellerud=true
-if [ -z "$gfile" ] || [ ! -f "$gfile" ]; then
-	echo 'Missing or unusable Gotland data file (-g file)'
-	echo 'Will not load Gotland data'
-	load_gotland=false
-fi >&2
-if "$load_gotland" && ( [ -z "$Gfile" ] || [ ! -f "$Gfile" ] ); then
-	echo 'Missing or unusable Gotland herd registry file (-G file)'
-	echo 'Will not load Gotland data'
-	load_gotland=false
-fi >&2
-if [ -z "$mfile" ] || [ ! -f "$mfile" ]; then
-	echo 'Missing or unusable Mellerud data file (-m file)' >&2
-	echo 'Will not load Mellerud data'
-	load_mellerud=false
-fi
-if [ -z "Mfile" ] || [ ! -f "$Mfile" ]; then
-	echo 'Missing or unusable Mellerud herd file (-M file)' >&2
-	echo 'Will not load extra Mellerud herd info'
-	load_mellerud_herds=false
-else
-    herd="-M /scripts/$Mfile"
-fi
-
-if docker-compose -f ../dc-db-load.yml ps | grep -q 'main-load-db.*\sUp\s.*'; then
-    # Alll good here
-    :
+if docker-compose -f ../dc-db-load.yml ps | grep -q 'main-load-db.*\sUp\s.*'
+then
+	: # All good
 else
     # Start containers and wait a little to give it time to start 
     echo "System is not up, bringing up before loading data"
@@ -82,7 +24,21 @@ else
     sleep 10
 fi
 
+# Add path "/scripts/" in front of each non-option argument.
+for opt do
+	case $opt in
+		-*) : ;;
+		*) opt="/scripts/$opt"
+	esac
+	set -- "$@" "$opt"
+	shift
+done
 
-docker-compose -f ../dc-db-load.yml exec main-load-db /scripts/load-in-docker.sh -g "/scripts/$gfile" -G "/scripts/$Gfile" -m "/scripts/$mfile" $herd
+docker-compose -f ../dc-db-load.yml \
+	exec main-load-db /scripts/load-in-docker.sh "$@"
 
-echo "Done you can stop the main-load-db container with docker-compose -f ../dc-db-load.yml stop"
+cat <<'END'
+Done.
+You can stop the main-load-db container with
+	docker-compose -f ../dc-db-load.yml stop
+END
