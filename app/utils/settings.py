4d7a0be257a7e9bf73bf293c8d00bbe9fa1f0725
jonandernovella <novella.uu@gmail.com>
2021-05-06 13:47:42 +0200

    Clean up to make linter happier

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -13,7 +13,7 @@ postgres = Namespace()  # pylint: disable=C0103
 rapi = Namespace()  # pylint: disable=C0103
 s3 = Namespace()  # pylint: disable=C0103
 certs = Namespace()  # pylint: disable=C0103
-service = Namespace() # pylint: disable=C0103
+service = Namespace()  # pylint: disable=C0103
 # Read configuration from environment variable
 
 certs.private_key = Path("/code/key.pem")
@@ -32,7 +32,7 @@ service.host = os.environ.get("HERDBOOK_HOST", "https://127.0.0.1:8443")
 
 s3.bucket = os.environ.get("S3_BUCKET", "test")
 s3.endpoint = os.environ.get("S3_BUCKET", "http://s3backend:9000")
-s3.region = os.environ.get("S3_REGION","us-east-1")
+s3.region = os.environ.get("S3_REGION", "us-east-1")
 s3.secret_key = os.environ.get("S3_SECRETKEY", "secretkeytest")
 s3.access_key = os.environ.get("S3_ACCESSKEY", "accesskeytest")
 s3.verify = False
