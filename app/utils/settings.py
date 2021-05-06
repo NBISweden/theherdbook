e974817d330b0f87863c27830fc212f731cb5156
jonandernovella <novella.uu@gmail.com>
2021-05-06 12:08:02 +0200

    Embed hostname in qr code

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -13,6 +13,7 @@ postgres = Namespace()  # pylint: disable=C0103
 rapi = Namespace()  # pylint: disable=C0103
 s3 = Namespace()  # pylint: disable=C0103
 certs = Namespace()  # pylint: disable=C0103
+service = Namespace() # pylint: disable=C0103
 # Read configuration from environment variable
 
 certs.private_key = Path("/code/key.pem")
@@ -26,6 +27,8 @@ postgres.password = os.environ.get("POSTGRES_PASSWORD", "insecure")
 
 rapi.host = os.environ.get("RAPI_HOST", "r-api")
 rapi.port = os.environ.get("RAPI_PORT", "31113")
+
+service.host = os.environ.get("HOSTNAME", "https://127.0.0.1:8443")
 
 s3.bucket = os.environ.get("S3_BUCKET", "test")
 s3.endpoint = os.environ.get("S3_BUCKET", "http://s3backend:9000")
