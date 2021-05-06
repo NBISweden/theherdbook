79e429ad99c5b24cc0f9f44d11710d90959f1d9e
Henrike <henrike.wiemker@t-online.de>
2021-05-06 14:16:17 +0200

    user prettier
    4 files

--- frontend/src/user_info.tsx
+++ frontend/src/user_info.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react'
+import * as React from "react";
 
-import {useUserContext} from '@app/user_context'
+import { useUserContext } from "@app/user_context";
 
 /** Component to write some info about the logged in user */
 export function UserInfo() {
-  const {user} = useUserContext()
-  return <>
-    Email: {user ? user.email : 'anonymous'}
-    {user ? <> ({user.validated ? 'validated' : 'not validated'})
-            </>
-          : ''}
-  </>
+  const { user } = useUserContext();
+  return (
+    <>
+      Email: {user ? user.email : "anonymous"}
+      {user ? <> ({user.validated ? "validated" : "not validated"})</> : ""}
+    </>
+  );
 }
