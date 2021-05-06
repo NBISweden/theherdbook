3000def2ea87a7de641ca5c57878135d8c0c8ba3
Henrike <henrike.wiemker@t-online.de>
2021-05-06 14:15:10 +0200

    pedigree, sorted, ui prettier
    4 files

--- frontend/src/pedigree.tsx
+++ frontend/src/pedigree.tsx
@@ -2,17 +2,27 @@
  * @file This file contains functions for calculating and formatting pedigrees.
  */
 
-import { Genebank, Individual, LimitedIndividual } from '@app/data_context_global';
+import {
+  Genebank,
+  Individual,
+  LimitedIndividual,
+} from "@app/data_context_global";
 
-type VisColor = {border: string, background: string,
-                 highlight: {border: string, background: string},
-                 hover: {border: string, background: string}
-                }
-export type Node = {id: string, x: number, label: string, shape: string,
-                    color: VisColor
-                  }
-export type Edge = {id: string, from: string, to: string}
-export type Pedigree = {nodes: Node[], edges: Edge[]}
+type VisColor = {
+  border: string;
+  background: string;
+  highlight: { border: string; background: string };
+  hover: { border: string; background: string };
+};
+export type Node = {
+  id: string;
+  x: number;
+  label: string;
+  shape: string;
+  color: VisColor;
+};
+export type Edge = { id: string; from: string; to: string };
+export type Pedigree = { nodes: Node[]; edges: Edge[] };
 
 /**
  * Returns a copy of the array with duplicates removed, via reference equality,
@@ -24,27 +34,27 @@ export type Pedigree = {nodes: Node[], edges: Edge[]}
  *
  */
 export function unique(xs: any[], key: string | undefined = undefined): any[] {
-  const seen = new Set()
-  return xs.filter(x => {
-    const duplicate = key ? seen.has(x[key]) : seen.has(x)
-    key ? seen.add(x[key]) : seen.add(x)
-    return !duplicate
-  })
+  const seen = new Set();
+  return xs.filter((x) => {
+    const duplicate = key ? seen.has(x[key]) : seen.has(x);
+    key ? seen.add(x[key]) : seen.add(x);
+    return !duplicate;
+  });
 }
 
-const indexes = new WeakMap<Genebank[], Record<string, Individual>>()
+const indexes = new WeakMap<Genebank[], Record<string, Individual>>();
 
 function get_index(genebanks: Genebank[]): Record<string, Individual> {
   if (!indexes.has(genebanks)) {
-    const index = {}
-    genebanks.forEach(genebank =>
-      (genebank.individuals || []).forEach(
-        i => {
-          index[i.number] = i
-        }))
-    indexes.set(genebanks, index)
+    const index = {};
+    genebanks.forEach((genebank) =>
+      (genebank.individuals || []).forEach((i) => {
+        index[i.number] = i;
+      })
+    );
+    indexes.set(genebanks, index);
   }
-  return indexes.get(genebanks)!
+  return indexes.get(genebanks)!;
 }
 
 /**
@@ -53,10 +63,13 @@ function get_index(genebanks: Genebank[]): Record<string, Individual> {
  *
  * @param id individual number of the animal to find
  */
-const getIndividual = (genebanks: Genebank[], id: string): Individual | undefined => {
-  const index = get_index(genebanks)
-  return index[id]
-}
+const getIndividual = (
+  genebanks: Genebank[],
+  id: string
+): Individual | undefined => {
+  const index = get_index(genebanks);
+  return index[id];
+};
 
 /**
  * Formats the limited individual information `ind` as a node descriptor with
@@ -67,38 +80,38 @@ const getIndividual = (genebanks: Genebank[], id: string): Individual | undefin
  */
 const asNode = (ind: Individual, x: number): Node => {
   const color: VisColor = {
-    border: 'darkgrey',
-    background: 'lightgreen',
+    border: "darkgrey",
+    background: "lightgreen",
     highlight: {
-      border: 'grey',
-      background: 'yellowgreen'
+      border: "grey",
+      background: "yellowgreen",
     },
     hover: {
-      border: 'black',
-      background: 'yellowgreen'
-    }
-  }
+      border: "black",
+      background: "yellowgreen",
+    },
+  };
   switch (ind.sex) {
-    case 'male':
-      color.background = 'LightSkyBlue'
-      color.highlight.background = '#6DC4F9'
-      color.hover.background = '#6DC4F9'
+    case "male":
+      color.background = "LightSkyBlue";
+      color.highlight.background = "#6DC4F9";
+      color.hover.background = "#6DC4F9";
       break;
-    case 'female':
-      color.background = 'pink'
-      color.highlight.background = '#FFA5B4'
-      color.hover.background = '#FFA5B4'
+    case "female":
+      color.background = "pink";
+      color.highlight.background = "#FFA5B4";
+      color.hover.background = "#FFA5B4";
       break;
   }
-  return {id: ind.number,
-          x: x,
-          label: ind.name ? `${ind.name}\n${ind.number}` : ind.number,
-          shape: ind.sex == 'male'   ? 'box'
-               : ind.sex == 'female' ? 'oval'
-                                     : 'triangle', // unknown sex
-          color: color
-        }
-}
+  return {
+    id: ind.number,
+    x: x,
+    label: ind.name ? `${ind.name}\n${ind.number}` : ind.number,
+    shape:
+      ind.sex == "male" ? "box" : ind.sex == "female" ? "oval" : "triangle", // unknown sex
+    color: color,
+  };
+};
 
 /**
  * Returns `generations` from a pedigree of an individual given by an `id`
@@ -108,93 +121,103 @@ const asNode = (ind: Individual, x: number): Node => {
  * @param id individual number of the individual whose pedigree to generate
  * @param generations the number of generations to plot
  */
-export function calcPedigree(genebanks: Genebank[], id: string, generations: number = 1000): Pedigree {
-  let nodes: Node[] = []
-  let edges: Edge[] = []
-  const indData = getIndividual(genebanks, id)
+export function calcPedigree(
+  genebanks: Genebank[],
+  id: string,
+  generations: number = 1000
+): Pedigree {
+  let nodes: Node[] = [];
+  let edges: Edge[] = [];
+  const indData = getIndividual(genebanks, id);
 
   if (indData) {
-    nodes.push( asNode(indData, edges.length) )
+    nodes.push(asNode(indData, edges.length));
 
     const getPedigree = (ind: Individual, level = generations) => {
-      let pedigreeNodes: Node[] = []
-      let pedigreeEdges: Edge[] = []
+      let pedigreeNodes: Node[] = [];
+      let pedigreeEdges: Edge[] = [];
       if (level > 0) {
         // loop over mother and father, if they exist
         [ind.mother, ind.father].forEach((parent: LimitedIndividual | null) => {
           if (parent && parent.id !== null) {
-            const data = getIndividual(genebanks, parent.number)
+            const data = getIndividual(genebanks, parent.number);
             if (data) {
-              pedigreeNodes.push(asNode(data, 0))
-              pedigreeEdges.push({id: `${ind.number}-${data.number}`,
-                                  from: ind.number,
-                                  to: data.number})
-              const parents = getPedigree(data, level - 1)
-              pedigreeNodes = [...pedigreeNodes, ...parents.nodes]
-              pedigreeEdges = [...pedigreeEdges, ...parents.edges]
+              pedigreeNodes.push(asNode(data, 0));
+              pedigreeEdges.push({
+                id: `${ind.number}-${data.number}`,
+                from: ind.number,
+                to: data.number,
+              });
+              const parents = getPedigree(data, level - 1);
+              pedigreeNodes = [...pedigreeNodes, ...parents.nodes];
+              pedigreeEdges = [...pedigreeEdges, ...parents.edges];
             }
           }
-        })
+        });
       }
-      return {nodes: pedigreeNodes, edges: pedigreeEdges}
-    }
-    const pedigree = getPedigree(indData)
-    nodes = [...nodes, ...pedigree.nodes]
-    edges = [...edges, ...pedigree.edges]
+      return { nodes: pedigreeNodes, edges: pedigreeEdges };
+    };
+    const pedigree = getPedigree(indData);
+    nodes = [...nodes, ...pedigree.nodes];
+    edges = [...edges, ...pedigree.edges];
   }
   // remove duplicate nodes and edges
-  nodes = unique(nodes, 'id')
-  edges = unique(edges, 'id')
+  nodes = unique(nodes, "id");
+  edges = unique(edges, "id");
 
-  nodes.sort((a, b) => a.id > b.id ? 1 : -1)
-  edges.sort((a, b) => a.id > b.id ? 1 : -1)
+  nodes.sort((a, b) => (a.id > b.id ? 1 : -1));
+  edges.sort((a, b) => (a.id > b.id ? 1 : -1));
 
-  return {nodes: nodes, edges: edges}
+  return { nodes: nodes, edges: edges };
 }
 
-export function calcPedigreeDan(genebanks: Genebank[], id: string, generations: number = 1000): Pedigree {
-  let nodes: Node[] = []
-  let edges: Edge[] = []
-  const indData = getIndividual(genebanks, id)
+export function calcPedigreeDan(
+  genebanks: Genebank[],
+  id: string,
+  generations: number = 1000
+): Pedigree {
+  let nodes: Node[] = [];
+  let edges: Edge[] = [];
+  const indData = getIndividual(genebanks, id);
 
-  const visited = {} as Record<string, number>
-  const queue = [{depth: 0, id}] as {depth: number, id: string}[]
-  let pos = 0
+  const visited = {} as Record<string, number>;
+  const queue = [{ depth: 0, id }] as { depth: number; id: string }[];
+  let pos = 0;
 
   while (pos < queue.length) {
-    const {depth, id} = queue[pos++]
+    const { depth, id } = queue[pos++];
     if (depth > generations) {
-      break
+      break;
     }
     if (visited[id] !== undefined) {
-      continue
+      continue;
     }
-    visited[id] = depth
-    const ind = getIndividual(genebanks, id)
+    visited[id] = depth;
+    const ind = getIndividual(genebanks, id);
     if (!ind) {
-      continue
+      continue;
     }
-    nodes.push(asNode(ind, 0))
-    const parents = [ind.mother, ind.father]
+    nodes.push(asNode(ind, 0));
+    const parents = [ind.mother, ind.father];
     parents.forEach((parent: LimitedIndividual | null) => {
       if (parent && parent.id !== null) {
         queue.push({
           depth: depth + 1,
           id: parent.number,
-        })
+        });
         edges.push({
           id: `${id}-${parent.number}`,
           from: id,
           to: parent.number,
-        })
+        });
       }
-    })
+    });
   }
 
-  nodes.sort((a, b) => a.id > b.id ? 1 : -1)
-  edges.sort((a, b) => a.id > b.id ? 1 : -1)
+  nodes.sort((a, b) => (a.id > b.id ? 1 : -1));
+  edges.sort((a, b) => (a.id > b.id ? 1 : -1));
 
-  return {nodes, edges}
+  return { nodes, edges };
 }
 
 /**
@@ -209,37 +232,48 @@ export function calcPedigreeDan(genebanks: Genebank[], id: string, generations: 
  * @param herdId the id of the herd to plot
  * @param generations the number of generations to plot
  */
-export function herdPedigree(genebanks: Genebank[], herdId: string | undefined, generations: number = 1000, algo = 'Martin' as 'Martin' | 'Dan'): Pedigree {
-
-  let nodes: Node[] = []
-  let edges: Edge[] = []
+export function herdPedigree(
+  genebanks: Genebank[],
+  herdId: string | undefined,
+  generations: number = 1000,
+  algo = "Martin" as "Martin" | "Dan"
+): Pedigree {
+  let nodes: Node[] = [];
+  let edges: Edge[] = [];
 
   if (herdId == undefined || !genebanks || genebanks.length == 0) {
-    return {nodes: [], edges: []}
+    return { nodes: [], edges: [] };
   }
 
-  const genebank = genebanks.find(genebank => genebank.herds.some(herd => herd.herd == herdId))
+  const genebank = genebanks.find((genebank) =>
+    genebank.herds.some((herd) => herd.herd == herdId)
+  );
   if (!genebank || !genebank.individuals) {
-    return {nodes: [], edges: []}
+    return { nodes: [], edges: [] };
   }
 
-  const herd = genebank.individuals.filter(individual => individual.herd.herd == herdId)
+  const herd = genebank.individuals.filter(
+    (individual) => individual.herd.herd == herdId
+  );
 
   if (herd.length == 0) {
-    return {nodes: [], edges: []}
+    return { nodes: [], edges: [] };
   }
 
-  herd.forEach(individual => {
-    const pedigree = (algo === 'Martin' ? calcPedigree : calcPedigreeDan)(genebanks, individual.number, generations)
+  herd.forEach((individual) => {
+    const pedigree = (algo === "Martin" ? calcPedigree : calcPedigreeDan)(
+      genebanks,
+      individual.number,
+      generations
+    );
 
-    nodes = [...nodes, ...pedigree.nodes]
-    edges = [...edges, ...pedigree.edges]
-
-  })
+    nodes = [...nodes, ...pedigree.nodes];
+    edges = [...edges, ...pedigree.edges];
+  });
 
   // remove duplicate nodes and edges
-  nodes = unique(nodes, 'id')
-  edges = unique(edges, 'id')
+  nodes = unique(nodes, "id");
+  edges = unique(edges, "id");
 
-  return {nodes: nodes, edges: edges}
+  return { nodes: nodes, edges: edges };
 }
--- frontend/src/ui_utils.tsx
+++ frontend/src/ui_utils.tsx
@@ -1,61 +1,72 @@
-import * as React from 'react'
+import * as React from "react";
 
 export interface MatchProps {
-  path?: string
-  exact?: boolean
-  strict?: boolean
-  sensitive?: boolean
+  path?: string;
+  exact?: boolean;
+  strict?: boolean;
+  sensitive?: boolean;
 }
 
 export interface RoutedTab extends MatchProps {
-  label: string
-  component?: React.ReactNode
-  visible?: boolean
-  icon?: React.ReactElement
-  on_click?: () => void
+  label: string;
+  component?: React.ReactNode;
+  visible?: boolean;
+  icon?: React.ReactElement;
+  on_click?: () => void;
 }
 
-type Arg0<T> = T extends (a: infer A) => any ? A : never
+type Arg0<T> = T extends (a: infer A) => any ? A : never;
 
-import * as Router from 'react-router-dom'
-import {Route} from 'react-router-dom'
-import {Tab, Tabs} from '@material-ui/core'
+import * as Router from "react-router-dom";
+import { Route } from "react-router-dom";
+import { Tab, Tabs } from "@material-ui/core";
 
-export function useRoutedTabs(routed_tabs: RoutedTab[], options?: Partial<{autoselect_first: boolean}>) {
-  const history = Router.useHistory()
-  const url = Router.useRouteMatch().url.replace(/\/$/, '')
+export function useRoutedTabs(
+  routed_tabs: RoutedTab[],
+  options?: Partial<{ autoselect_first: boolean }>
+) {
+  const history = Router.useHistory();
+  const url = Router.useRouteMatch().url.replace(/\/$/, "");
 
   const matchprops = (tab: RoutedTab): MatchProps => {
-    const {path, exact, strict, sensitive} = tab
-    return {path: url + path, exact, strict, sensitive}
-  }
+    const { path, exact, strict, sensitive } = tab;
+    return { path: url + path, exact, strict, sensitive };
+  };
 
-  const location = Router.useLocation()
-  const tab_index = routed_tabs.findIndex(tab =>
+  const location = Router.useLocation();
+  const tab_index = routed_tabs.findIndex((tab) =>
     Router.matchPath(location.pathname, matchprops(tab))
-  )
+  );
 
-  const set_tab = (index: number | ((index: number, num_tabs: number) => number)) => {
-    const next_index = typeof index == 'function' ? index(tab_index, routed_tabs.length) : index
-    const tab = routed_tabs[next_index]
+  const set_tab = (
+    index: number | ((index: number, num_tabs: number) => number)
+  ) => {
+    const next_index =
+      typeof index == "function" ? index(tab_index, routed_tabs.length) : index;
+    const tab = routed_tabs[next_index];
     if (tab.on_click) {
-      tab.on_click()
+      tab.on_click();
     }
     if (tab.path) {
-      history.push(url + tab.path)
+      history.push(url + tab.path);
     }
-  }
+  };
 
   if (tab_index == -1) {
     if (options && options.autoselect_first && routed_tabs.length) {
-      set_tab(0)
+      set_tab(0);
     } else {
-      console.warn('Route', location.pathname, 'has no corresponding tab from', url)
+      console.warn(
+        "Route",
+        location.pathname,
+        "has no corresponding tab from",
+        url
+      );
     }
   }
 
   return {
-    Tabs: (props?: Partial<Arg0<typeof Tabs>>) =>
+    Tabs: (props?: Partial<Arg0<typeof Tabs>>) => (
       <Tabs
         indicatorColor="primary"
         textColor="primary"
@@ -63,34 +74,44 @@ export function useRoutedTabs(routed_tabs: RoutedTab[], options?: Partial<{autos
         value={tab_index === -1 ? false : tab_index}
         onChange={(_, index) => set_tab(index)}
       >
-        {routed_tabs.map((tab, i) =>
-          <Tab key={i} icon={tab.icon} label={tab.label} style={{display: tab.visible === false ? 'none' : undefined}}/>
-        )}
-      </Tabs>,
-    TabbedRoutes:
-      routed_tabs.map((tab, i) =>
-        tab.path && tab.component && <Route {...matchprops(tab)} key={i}>
-          {tab.component}
-        </Route>),
+        {routed_tabs.map((tab, i) => (
+          <Tab
+            key={i}
+            icon={tab.icon}
+            label={tab.label}
+            style={{ display: tab.visible === false ? "none" : undefined }}
+          />
+        ))}
+      </Tabs>
+    ),
+    TabbedRoutes: routed_tabs.map(
+      (tab, i) =>
+        tab.path &&
+        tab.component && (
+          <Route {...matchprops(tab)} key={i}>
+            {tab.component}
+          </Route>
+        )
+    ),
     tab_index,
     set_tab,
-  }
+  };
 }
 
-function RoutedInner(props: {children: (params: Record<string, string>) => React.ReactNode}) {
-  const params = Router.useParams()
-  return <>
-    {props.children(params)}
-  </>
+function RoutedInner(props: {
+  children: (params: Record<string, string>) => React.ReactNode;
+}) {
+  const params = Router.useParams();
+  return <>{props.children(params)}</>;
 }
 
-export function Routed(props: {path: string, children: (params: Record<string, string>) => React.ReactNode}) {
+export function Routed(props: {
+  path: string;
+  children: (params: Record<string, string>) => React.ReactNode;
+}) {
   return (
     <Route path={props.path}>
-      <RoutedInner>
-        {props.children}
-      </RoutedInner>
+      <RoutedInner>{props.children}</RoutedInner>
     </Route>
-  )
+  );
 }
-
