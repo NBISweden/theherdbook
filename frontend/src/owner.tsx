77862731cac19ba06934a25441e00eb0e5d0b841
Henrike <henrike.wiemker@t-online.de>
2021-05-06 14:13:59 +0200

    login, main, manage, message, nav., owner prettier
    6 files

--- frontend/src/owner.tsx
+++ frontend/src/owner.tsx
@@ -2,54 +2,66 @@
  * @file The Owner function provides a herd owner interfaces for managing
  * breeding events, registering new animals, and updating animal information.
  */
-import React, { useState } from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import { useUserContext } from '@app/user_context'
-import { Button, Paper } from '@material-ui/core'
-import { HerdView } from '@app/herd_view';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useUserContext } from "@app/user_context";
+import { Button, Paper } from "@material-ui/core";
+import { HerdView } from "@app/herd_view";
 
 const useStyles = makeStyles({
   container: {
     padding: "20px",
   },
   title: {
-    fontSize: '1.33em',
-    fontWeight: 'bold',
+    fontSize: "1.33em",
+    fontWeight: "bold",
   },
   herdButton: {
-    margin: '0 10px',
-  }
+    margin: "0 10px",
+  },
 });
 
 /**
  * Loads herd information from the backend and displays it as editable fields.
  */
 export function Owner() {
-  const { user } = useUserContext()
-  const [ activeHerd, setActiveHerd ] = useState((user?.is_owner && user.is_owner.length > 0
-                                                  ? user.is_owner[0]
-                                                  : undefined) as string | undefined)
-  const style  = useStyles()
+  const { user } = useUserContext();
+  const [activeHerd, setActiveHerd] = useState(
+    (user?.is_owner && user.is_owner.length > 0
+      ? user.is_owner[0]
+      : undefined) as string | undefined
+  );
+  const style = useStyles();
 
-  return <>
-    <Paper className={style.container}>
-      {user?.is_owner && user.is_owner.length > 1
-        ? <p className={style.title}>Mina Besättningar:
-            {user.is_owner.map((herdId: string) => <Button
+  return (
+    <>
+      <Paper className={style.container}>
+        {user?.is_owner && user.is_owner.length > 1 ? (
+          <p className={style.title}>
+            Mina Besättningar:
+            {user.is_owner.map((herdId: string) => (
+              <Button
                 className={style.herdButton}
                 key={herdId}
-                variant='contained'
+                variant="contained"
                 value={herdId}
-                color={herdId == activeHerd ? 'primary' : 'default'}
-                onClick={e => setActiveHerd(e.currentTarget.value)}>
-                  {herdId}
-              </Button>)}
+                color={herdId == activeHerd ? "primary" : "default"}
+                onClick={(e) => setActiveHerd(e.currentTarget.value)}
+              >
+                {herdId}
+              </Button>
+            ))}
           </p>
-        : <p className={style.title}>
-            Min Besättning
-          </p>
-      }
-    </Paper>
-    {React.useMemo(() => <HerdView id={activeHerd} />, [activeHerd])}
-  </>
+        ) : (
+          <p className={style.title}>Min Besättning</p>
+        )}
+      </Paper>
+      {React.useMemo(
+        () => (
+          <HerdView id={activeHerd} />
+        ),
+        [activeHerd]
+      )}
+    </>
+  );
 }
