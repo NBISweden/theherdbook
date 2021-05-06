bbe314d9888fd984096c8d7c341a46a5240e7e4a
Henrike <henrike.wiemker@t-online.de>
2021-05-06 14:12:54 +0200

    herd and individual files prettier
    6 files

--- frontend/src/herd_view.tsx
+++ frontend/src/herd_view.tsx
@@ -3,35 +3,42 @@
  *       for a given `id` (parsed from the url), as well as the individuals
  *       belonging to that herd.
  */
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Box, CircularProgress, Paper, Tab, Tabs } from '@material-ui/core';
-import { get } from '@app/communication';
-import { Herd, Individual } from '@app/data_context_global';
-import { HerdForm } from '@app/herdForm';
-import { useMessageContext } from '@app/message_context';
-import { useDataContext } from '@app/data_context';
-import { herdPedigree } from '@app/pedigree';
-import { PedigreeNetwork } from '@app/pedigree_plot';
-import { FilterTable } from '@app/filter_table';
-import { IndividualView } from '@app/individual_view';
-import { IndividualEdit } from '@app/individual_edit';
-import { useUserContext } from '@app/user_context';
-import { BreedingList } from './breeding_list';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Box,
+  CircularProgress,
+  Paper,
+  Tab,
+  Tabs,
+} from "@material-ui/core";
+import { get } from "@app/communication";
+import { Herd, Individual } from "@app/data_context_global";
+import { HerdForm } from "@app/herdForm";
+import { useMessageContext } from "@app/message_context";
+import { useDataContext } from "@app/data_context";
+import { herdPedigree } from "@app/pedigree";
+import { PedigreeNetwork } from "@app/pedigree_plot";
+import { FilterTable } from "@app/filter_table";
+import { IndividualView } from "@app/individual_view";
+import { IndividualEdit } from "@app/individual_edit";
+import { useUserContext } from "@app/user_context";
+import { BreedingList } from "./breeding_list";
 
 const useStyles = makeStyles({
   container: {
     padding: "20px",
   },
   title: {
-    fontSize: '1.33em',
-    fontWeight: 'bold',
+    fontSize: "1.33em",
+    fontWeight: "bold",
   },
   animalList: {
-    cursor: 'pointer',
-    '&:hover': {
-      color: 'blue',
-    }
+    cursor: "pointer",
+    "&:hover": {
+      color: "blue",
+    },
   },
   loading: {
     display: "flex",
@@ -41,7 +48,11 @@ const useStyles = makeStyles({
   },
 });
 
-interface TabPanelProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement> {
+interface TabPanelProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
   children?: React.ReactNode;
   index: any;
   value: any;
@@ -58,112 +69,134 @@ function TabPanel(props: TabPanelProps) {
       aria-labelledby={`full-width-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box>
-          {children}
-        </Box>
-      )}
+      {value === index && <Box>{children}</Box>}
     </div>
   );
 }
 
-type TabValue = 'list' | 'pedigree';
+type TabValue = "list" | "pedigree";
 
 /**
  * Shows herd information, with a list of all individuals belonging to that
  * herd.
  */
-export function HerdView({id}: {id: string | undefined}) {
-  const [herd, setHerd] = React.useState(undefined as Herd | undefined)
-  const [herdIndividuals, setHerdIndividuals] = React.useState([] as Individual[])
-  const [activeTab, setActiveTab] = React.useState('list' as TabValue)
-  const {userMessage, popup} = useMessageContext()
-  const { user } = useUserContext()
-  const { genebanks } = useDataContext()
-  const [algo, set_algo] = React.useState('Martin' as 'Martin' | 'Dan')
-  const pedigree = React.useMemo(() => herdPedigree(genebanks, id, 5, algo), [genebanks, id, algo])
-  const style  = useStyles()
+export function HerdView({ id }: { id: string | undefined }) {
+  const [herd, setHerd] = React.useState(undefined as Herd | undefined);
+  const [herdIndividuals, setHerdIndividuals] = React.useState(
+    [] as Individual[]
+  );
+  const [activeTab, setActiveTab] = React.useState("list" as TabValue);
+  const { userMessage, popup } = useMessageContext();
+  const { user } = useUserContext();
+  const { genebanks } = useDataContext();
+  const [algo, set_algo] = React.useState("Martin" as "Martin" | "Dan");
+  const pedigree = React.useMemo(() => herdPedigree(genebanks, id, 5, algo), [
+    genebanks,
+    id,
+    algo,
+  ]);
+  const style = useStyles();
 
   React.useEffect(() => {
     if (id) {
       get(`/api/herd/${id}`).then(
         (data: Herd) => data && setHerd(data),
-        error => {
-          console.error(error)
-          userMessage(error, 'error')
+        (error) => {
+          console.error(error);
+          userMessage(error, "error");
         }
-      )
+      );
     }
-  }, [id])
+  }, [id]);
 
   React.useEffect(() => {
     if (herd && genebanks && herd.individuals) {
-      const genebank = genebanks.find(g => g.herds.some(h => h.herd == herd.herd))
-      const individualIds = herd.individuals.map(i => i.number)
+      const genebank = genebanks.find((g) =>
+        g.herds.some((h) => h.herd == herd.herd)
+      );
+      const individualIds = herd.individuals.map((i) => i.number);
       if (genebank && genebank.individuals != null) {
-        const individualsList = genebank.individuals.filter(i => individualIds.includes(i.number))
-        setHerdIndividuals(individualsList)
+        const individualsList = genebank.individuals.filter((i) =>
+          individualIds.includes(i.number)
+        );
+        setHerdIndividuals(individualsList);
       }
     }
-  }, [herd, genebanks])
+  }, [herd, genebanks]);
 
-  return <>
-    <Paper className={style.container}>
-      {React.useMemo(() => <HerdForm id={id} view='info'/>, [id])}
+  return (
+    <>
+      <Paper className={style.container}>
+        {React.useMemo(
+          () => (
+            <HerdForm id={id} view="info" />
+          ),
+          [id]
+        )}
 
-      <AppBar position="static" color="default">
-        <Tabs
-          value={activeTab}
-          onChange={(event: React.ChangeEvent<{}>, newValue: TabValue) => {
-            setActiveTab(newValue)
-          }}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="fullWidth"
+        <AppBar position="static" color="default">
+          <Tabs
+            value={activeTab}
+            onChange={(event: React.ChangeEvent<{}>, newValue: TabValue) => {
+              setActiveTab(newValue);
+            }}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
           >
-            <Tab label="Lista över individer" value='list'/>
-            <Tab label="Släktträd för besättningen" value='pedigree'/>
-        </Tabs>
-      </AppBar>
+            <Tab label="Lista över individer" value="list" />
+            <Tab label="Släktträd för besättningen" value="pedigree" />
+          </Tabs>
+        </AppBar>
 
-      <TabPanel value={activeTab} index='list'>
-        {herdIndividuals.length ?
-          <FilterTable
-            individuals={herdIndividuals}
-            title={'Individer i besättningen'}
-            filters={[{field: 'alive', label: 'Dölj döda'},
-                      {field: 'active', label: 'Dölj inaktiva djur'}]}
-            action={user?.canEdit(id)
-                      ? (event: any, rowData: any) => {
-                          popup(<IndividualEdit id={rowData.number} />)
-                        }
-                      : undefined}
+        <TabPanel value={activeTab} index="list">
+          {herdIndividuals.length ? (
+            <FilterTable
+              individuals={herdIndividuals}
+              title={"Individer i besättningen"}
+              filters={[
+                { field: "alive", label: "Dölj döda" },
+                { field: "active", label: "Dölj inaktiva djur" },
+              ]}
+              action={
+                user?.canEdit(id)
+                  ? (event: any, rowData: any) => {
+                      popup(<IndividualEdit id={rowData.number} />);
+                    }
+                  : undefined
+              }
             />
-          :
-          <div className={style.loading}>
-            <h2>Loading Individuals</h2>
-            <CircularProgress />
+          ) : (
+            <div className={style.loading}>
+              <h2>Loading Individuals</h2>
+              <CircularProgress />
+            </div>
+          )}
+        </TabPanel>
+        <TabPanel value={activeTab} index="pedigree">
+          <div style={{ marginTop: 10, display: "flex" }}>
+            <label style={{ margin: "auto" }}>
+              Algorithm:
+              <select
+                style={{ marginLeft: 10 }}
+                onChange={(e) => e.target && set_algo(e.target.value as any)}
+              >
+                <option value="Martin">Martin</option>
+                <option value="Dan">Dan</option>
+              </select>
+            </label>
           </div>
-        }
-      </TabPanel>
-      <TabPanel value={activeTab} index='pedigree'>
-        <div style={{marginTop: 10, display: 'flex'}}>
-          <label style={{margin: 'auto'}}>
-            Algorithm:
-            <select style={{marginLeft: 10}} onChange={e => e.target && set_algo(e.target.value as any)} >
-              <option value="Martin">Martin</option>
-              <option value="Dan">Dan</option>
-            </select>
-          </label>
-        </div>
-        {pedigree &&
-          <PedigreeNetwork
-            pedigree={pedigree}
-            onClick={(node: string) => popup(<IndividualView id={node} />, `/individual/${node}`)}
-          />
-        }
-      </TabPanel>
-      <BreedingList id={id}/>
-    </Paper>
-  </>
+          {pedigree && (
+            <PedigreeNetwork
+              pedigree={pedigree}
+              onClick={(node: string) =>
+                popup(<IndividualView id={node} />, `/individual/${node}`)
+              }
+            />
+          )}
+        </TabPanel>
+        <BreedingList id={id} />
+      </Paper>
+    </>
+  );
 }
--- frontend/src/individual_view.tsx
+++ frontend/src/individual_view.tsx
@@ -2,91 +2,110 @@
  * @file The IndividualView function provides information about a single
  * individual, including links to parents and progeny.
  */
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Tooltip} from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
-import { useMessageContext } from '@app/message_context'
-import { get } from '@app/communication'
-import { Individual, herdLabel, DateValue, individualLabel, asLocale } from '@app/data_context_global'
-import { useDataContext } from '@app/data_context'
-import { IndividualPedigree } from '@app/individual_pedigree'
-import { useUserContext } from '@app/user_context'
-import { IndividualEdit } from '@app/individual_edit'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Tooltip } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useMessageContext } from "@app/message_context";
+import { get } from "@app/communication";
+import {
+  Individual,
+  herdLabel,
+  DateValue,
+  individualLabel,
+  asLocale,
+} from "@app/data_context_global";
+import { useDataContext } from "@app/data_context";
+import { IndividualPedigree } from "@app/individual_pedigree";
+import { useUserContext } from "@app/user_context";
+import { IndividualEdit } from "@app/individual_edit";
 
 const useStyles = makeStyles({
   body: {
-    display: 'flex',
-    flexDirection: 'row',
+    display: "flex",
+    flexDirection: "row",
   },
   herdList: {
-    listStyle: 'none',
-    padding: '0 0 0 10px',
-    margin: '0 0 0 5px',
-    borderLeft: '1px solid lightgrey',
+    listStyle: "none",
+    padding: "0 0 0 10px",
+    margin: "0 0 0 5px",
+    borderLeft: "1px solid lightgrey",
   },
   fillWidth: {
-    width: '100%',
+    width: "100%",
   },
   flexColumn: {
-    minWidth: '300px',
-    display: 'flex',
-    flexDirection: 'column',
+    minWidth: "300px",
+    display: "flex",
+    flexDirection: "column",
   },
   sameLine: {
-    width: '100%',
-    overflow: 'hidden',
-    padding: '0 0 0 10px',
-    margin: '0 0 0 5px',
-    borderLeft: '1px solid lightgrey',
-    '& dt, dd': {
-      display: 'block',
-      float: 'left',
+    width: "100%",
+    overflow: "hidden",
+    padding: "0 0 0 10px",
+    margin: "0 0 0 5px",
+    borderLeft: "1px solid lightgrey",
+    "& dt, dd": {
+      display: "block",
+      float: "left",
     },
-    '& dt': {
-      width: '50px',
-      clear: 'both',
-    }
+    "& dt": {
+      width: "50px",
+      clear: "both",
+    },
   },
   listIcon: {
-    display: 'inline-block',
-    width: '20px',
-    textAlign: 'center',
+    display: "inline-block",
+    width: "20px",
+    textAlign: "center",
   },
   editButton: {
-    marginTop: '15px',
-  }
+    marginTop: "15px",
+  },
 });
 
 /**
  * Loads information for an individual from the backend and displays it.
  */
-export function IndividualView({id} : {id: string}) {
-  const style  = useStyles()
-  const { genebanks } = useDataContext()
-  const { user } = useUserContext()
-  const [individual, setIndividual] = React.useState(undefined as Individual | undefined)
-  const {userMessage, popup} = useMessageContext()
+export function IndividualView({ id }: { id: string }) {
+  const style = useStyles();
+  const { genebanks } = useDataContext();
+  const { user } = useUserContext();
+  const [individual, setIndividual] = React.useState(
+    undefined as Individual | undefined
+  );
+  const { userMessage, popup } = useMessageContext();
 
   const children: Individual[] = React.useMemo(() => {
-    if (!individual || !genebanks || genebanks.some(g => g.individuals == null)) {
-      return []
+    if (
+      !individual ||
+      !genebanks ||
+      genebanks.some((g) => g.individuals == null)
+    ) {
+      return [];
     }
-    const genebank = genebanks.find(genebank =>
-      genebank.individuals.some(i => i.number == individual.number)
-    )
-    if (!genebank || genebank.individuals == null || !genebank.individuals.length) {
-      return []
+    const genebank = genebanks.find((genebank) =>
+      genebank.individuals.some((i) => i.number == individual.number)
+    );
+    if (
+      !genebank ||
+      genebank.individuals == null ||
+      !genebank.individuals.length
+    ) {
+      return [];
     }
-    return genebank.individuals.filter(i =>
-      (i.mother && i.mother.number == individual.number) ||
-      (i.father && i.father.number == individual.number)
-    ) ?? []
-  }, [individual, genebanks])
+    return (
+      genebank.individuals.filter(
+        (i) =>
+          (i.mother && i.mother.number == individual.number) ||
+          (i.father && i.father.number == individual.number)
+      ) ?? []
+    );
+  }, [individual, genebanks]);
 
-  const activeIcon = '✅'
-  const inactiveIcon = '❌'
-  const deadIcon = '✞'
+  const activeIcon = "✅";
+  const inactiveIcon = "❌";
+  const deadIcon = "✞";
 
   /**
    * Fetch individual data from the backend
@@ -94,138 +113,175 @@ export function IndividualView({id} : {id: string}) {
   React.useEffect(() => {
     get(`/api/individual/${id}`).then(
       (data: Individual) => setIndividual(data),
-      error => {
+      (error) => {
         console.error(error);
-        userMessage(error, 'error')
+        userMessage(error, "error");
       }
-    )
-  }, [id])
+    );
+  }, [id]);
 
-  return <>
-    <div className={style.body}>
-      { individual ? <>
-        <div className={style.flexColumn}>
-          <div>
-            <h3>{individual?.name ?? individual.number}</h3>
-            <dl className={style.sameLine}>
-              <dt>Namn:</dt><dd>{individual?.name}</dd>
-              <dt>Nummer:</dt><dd>{individual?.number}</dd>
-              <dt>Certifikat:</dt><dd>{individual?.certificate}</dd>
-              <dt>Kön:</dt><dd>{individual?.sex}</dd>
-              <dt><Tooltip title="Inavelskoefficient"><span>F:</span></Tooltip></dt><dd>{individual?.inbreeding}%</dd>
-              <dt><Tooltip title="Genomsnittligt släktskap/ Mean Kinship"><span>MK:</span></Tooltip></dt><dd>{individual?.MK}%</dd>
-              <dt>Färg:</dt><dd>
-                {individual?.color}<br />
-                {individual?.color_note}
-              </dd>
-              <dt>Född:</dt><dd>
-                {individual?.birth_date
-                 ? asLocale(individual.birth_date)
-                 : '-'
-                 }
-                {individual?.death_date
-                 ? ` - Död: ${asLocale(individual.death_date)}`
-                 : ''
-                 }
-              </dd>
-              <dt>Kullstorlek:</dt><dd>{individual?.litter}</dd>
-              <dt>Vikt:</dt>
-              <dd>
-                {individual && individual.weights && individual.weights.length > 1
-                  && <ul className={style.herdList}>
-                    {individual.weights.sort((a: DateValue, b: DateValue) =>
-                                              new Date(a.date).getTime() - new Date(b.date).getTime())
-                                      .map((w: any, i) => {
-                                        return <li key={i}>
-                                          {`${asLocale(w.date)}: ${w.weight} kg`}
-                                        </li>
-                                      })
-                    }
-                  </ul>
-                }
-              </dd>
-              <dt>Anteckningar</dt><dd>{individual?.notes ?? '-'}</dd>
-            </dl>
-          </div>
-          {user?.canEdit(id) &&
-            <Button className={style.editButton}
-                    variant="contained"
-                    color="primary"
-                    onClick={() => popup(<IndividualEdit id={id} />)}
-              >
-              Redigera individ
-            </Button>
-          }
-          <div>
-            <h3>Besättningshistoria</h3>
-            <ul className={style.herdList}>
-
-              {individual && individual.herd_tracking && individual.herd_tracking.map((herdTrack: any, i) => {
-                if (herdTrack.herd) {
-                  return <Link to={`/herd/${herdTrack.herd}`} key={i}>
-                            <li>{asLocale(herdTrack.date)}: {herdLabel(herdTrack)}</li>
+  return (
+    <>
+      <div className={style.body}>
+        {individual ? (
+          <>
+            <div className={style.flexColumn}>
+              <div>
+                <h3>{individual?.name ?? individual.number}</h3>
+                <dl className={style.sameLine}>
+                  <dt>Namn:</dt>
+                  <dd>{individual?.name}</dd>
+                  <dt>Nummer:</dt>
+                  <dd>{individual?.number}</dd>
+                  <dt>Certifikat:</dt>
+                  <dd>{individual?.certificate}</dd>
+                  <dt>Kön:</dt>
+                  <dd>{individual?.sex}</dd>
+                  <dt>
+                    <Tooltip title="Inavelskoefficient">
+                      <span>F:</span>
+                    </Tooltip>
+                  </dt>
+                  <dd>{individual?.inbreeding}%</dd>
+                  <dt>
+                    <Tooltip title="Genomsnittligt släktskap/ Mean Kinship">
+                      <span>MK:</span>
+                    </Tooltip>
+                  </dt>
+                  <dd>{individual?.MK}%</dd>
+                  <dt>Färg:</dt>
+                  <dd>
+                    {individual?.color}
+                    <br />
+                    {individual?.color_note}
+                  </dd>
+                  <dt>Född:</dt>
+                  <dd>
+                    {individual?.birth_date
+                      ? asLocale(individual.birth_date)
+                      : "-"}
+                    {individual?.death_date
+                      ? ` - Död: ${asLocale(individual.death_date)}`
+                      : ""}
+                  </dd>
+                  <dt>Kullstorlek:</dt>
+                  <dd>{individual?.litter}</dd>
+                  <dt>Vikt:</dt>
+                  <dd>
+                    {individual &&
+                      individual.weights &&
+                      individual.weights.length > 1 && (
+                        <ul className={style.herdList}>
+                          {individual.weights
+                            .sort(
+                              (a: DateValue, b: DateValue) =>
+                                new Date(a.date).getTime() -
+                                new Date(b.date).getTime()
+                            )
+                            .map((w: any, i) => {
+                              return (
+                                <li key={i}>
+                                  {`${asLocale(w.date)}: ${w.weight} kg`}
+                                </li>
+                              );
+                            })}
+                        </ul>
+                      )}
+                  </dd>
+                  <dt>Anteckningar</dt>
+                  <dd>{individual?.notes ?? "-"}</dd>
+                </dl>
+              </div>
+              {user?.canEdit(id) && (
+                <Button
+                  className={style.editButton}
+                  variant="contained"
+                  color="primary"
+                  onClick={() => popup(<IndividualEdit id={id} />)}
+                >
+                  Redigera individ
+                </Button>
+              )}
+              <div>
+                <h3>Besättningshistoria</h3>
+                <ul className={style.herdList}>
+                  {individual &&
+                    individual.herd_tracking &&
+                    individual.herd_tracking.map((herdTrack: any, i) => {
+                      if (herdTrack.herd) {
+                        return (
+                          <Link to={`/herd/${herdTrack.herd}`} key={i}>
+                            <li>
+                              {asLocale(herdTrack.date)}: {herdLabel(herdTrack)}
+                            </li>
                           </Link>
-                } else {
-                  return <li key={i}>{herdTrack.date}: Utanför genbanken</li>
-                }
-
-              })}
-            </ul>
-          </div>
-          <div>
-            <h3>Föräldrar</h3>
-            {individual &&
-              <ul className={style.herdList}>
-                <li>
-                  Mor:
-                  {individual.mother
-                  ? <Link to={`/individual/${individual.mother.number}`} >
-                      {individualLabel(individual.mother)}
-                    </Link>
-                  : 'Okänd'
-                  }
-                </li>
-                <li>
-                  Far:
-                  {individual.father
-                  ? <Link to={`/individual/${individual.father.number}`} >
-                      {individualLabel(individual.father)}
-                    </Link>
-                  : 'Okänd'
-                  }
-                </li>
-              </ul>
-            }
-          </div>
-          <div>
-            <h3>Avkomma</h3>
-            <h4>Avkomma i genbanken</h4>
-            ({activeIcon}: Aktiv, {inactiveIcon}: Inaktiv, {deadIcon}: Död)
-            <ul className={style.herdList}>
-              {children.map(child =>
-                  <li key={child.number}>
-                    <span className={style.listIcon}>
-                      {child.alive ? child.active ? activeIcon
-                                                  : inactiveIcon
-                                  : deadIcon
+                        );
+                      } else {
+                        return (
+                          <li key={i}>{herdTrack.date}: Utanför genbanken</li>
+                        );
                       }
-                    </span>
-                    <Link to={`/individual/${child.number}`} >
-                      {individualLabel(child)}
-                    </Link>
-                  </li>
-                )
-              }
-            </ul>
-          </div>
+                    })}
+                </ul>
+              </div>
+              <div>
+                <h3>Föräldrar</h3>
+                {individual && (
+                  <ul className={style.herdList}>
+                    <li>
+                      Mor:
+                      {individual.mother ? (
+                        <Link to={`/individual/${individual.mother.number}`}>
+                          {individualLabel(individual.mother)}
+                        </Link>
+                      ) : (
+                        "Okänd"
+                      )}
+                    </li>
+                    <li>
+                      Far:
+                      {individual.father ? (
+                        <Link to={`/individual/${individual.father.number}`}>
+                          {individualLabel(individual.father)}
+                        </Link>
+                      ) : (
+                        "Okänd"
+                      )}
+                    </li>
+                  </ul>
+                )}
+              </div>
+              <div>
+                <h3>Avkomma</h3>
+                <h4>Avkomma i genbanken</h4>({activeIcon}: Aktiv, {inactiveIcon}
+                : Inaktiv, {deadIcon}: Död)
+                <ul className={style.herdList}>
+                  {children.map((child) => (
+                    <li key={child.number}>
+                      <span className={style.listIcon}>
+                        {child.alive
+                          ? child.active
+                            ? activeIcon
+                            : inactiveIcon
+                          : deadIcon}
+                      </span>
+                      <Link to={`/individual/${child.number}`}>
+                        {individualLabel(child)}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </>
+        ) : (
+          "Loading"
+        )}
+        <div className={style.fillWidth}>
+          <h3>Släktträd</h3>
+          <IndividualPedigree id={id} generations={3}></IndividualPedigree>
         </div>
-      </>
-      : 'Loading'
-      }
-      <div className={style.fillWidth}>
-        <h3>Släktträd</h3>
-        <IndividualPedigree id={id} generations={3}></IndividualPedigree>
       </div>
-    </div>
-  </>
+    </>
+  );
 }
