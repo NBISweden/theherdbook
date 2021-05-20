1622dffae622b1cab9773538e169dd4225cf10a8
Henrike <henrike.wiemker@t-online.de>
2021-05-20 13:41:11 +0200

    Basic layout for certificate verification
    also started to write function for uploading. Still not functional

--- frontend/src/certificate_verification.tsx
+++ frontend/src/certificate_verification.tsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Button, CircularProgress } from "@material-ui/core";
 
 import { Individual } from "@app/data_context_global";
+import { styles } from "@material-ui/pickers/views/Calendar/Calendar";
 
 const useStyles = makeStyles({
   titleText: {
@@ -10,6 +11,21 @@ const useStyles = makeStyles({
     borderBottom: "1px solid lightgrey",
     padding: "0 20px",
     fontSize: "2.3em",
+  },
+  fileInput: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  label: {
+    margin: "20px 20px 20px 0",
+    marginRight: "20px",
+  },
+  flexRow: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    margin: "20px 0",
   },
 });
 
@@ -20,9 +36,19 @@ export function CertificateVerification({
   id: string;
   individual: Individual | undefined;
 }) {
+  const [file, setFile] = React.useState(undefined as File | undefined);
   const style = useStyles();
 
-  const verifyCertificate = (id: string) => {};
+  const verifyCertificate = (id: string) => {
+    fetch(`/api/certificates/verify/${id}`, {
+      body: file,
+      method: "POST",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
   return (
     <>
       <h1>Verifiera certifikat</h1>
@@ -30,14 +56,29 @@ export function CertificateVerification({
         Ladda upp ett digitalt certifikat för att se att det är signerat och
         finns i databasen.
       </p>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => verifyCertificate(id)}
-      >
-        Ladda upp certifikat
-        <input type="file" hidden></input>
-      </Button>
+      <div className={style.fileInput}>
+        <input
+          id="inputFile"
+          type="file"
+          onChange={(event) => setFile(event.target.files[0])}
+          style={{ display: "none" }}
+        />
+        <label htmlFor="inputFile" className={style.label}>
+          <Button variant="contained" color="primary" component="span">
+            Välj fil...
+          </Button>
+        </label>
+        {file ? <p>{file.name}</p> : <p>Ingen fil vald.</p>}
+      </div>
+      <div className={style.flexRow}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => console.log(file)}
+        >
+          Verifiera
+        </Button>
+      </div>
     </>
   );
 }
