9001723c37a7cf33201a596b90a42437958f5a7a
jonandernovella <novella.uu@gmail.com>
2021-05-24 19:55:25 +0200

    Handle S3 related vars properly

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -31,7 +31,7 @@ rapi.port = os.environ.get("RAPI_PORT", "31113")
 service.host = os.environ.get("HERDBOOK_HOST", "https://127.0.0.1:8443")
 
 s3.bucket = os.environ.get("S3_BUCKET", "test")
-s3.endpoint = os.environ.get("S3_BUCKET", "http://s3backend:9000")
+s3.endpoint = os.environ.get("S3_ENDPOINT", None)
 s3.region = os.environ.get("S3_REGION", "us-east-1")
 s3.secret_key = os.environ.get("S3_SECRETKEY", "secretkeytest")
 s3.access_key = os.environ.get("S3_ACCESSKEY", "accesskeytest")
