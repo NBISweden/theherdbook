e93c9d9de686f6e281a93c00156567b41e74ef9b
Henrike <henrike.wiemker@t-online.de>
2021-06-02 10:36:59 +0200

    create component to add individual

---
+++ frontend/src/individual_add.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import { CertificateForm } from "@app/certificate_form";
+
+export function IndividualAdd() {
+  return (
+    <>
+      <h1>Fyll i uppgifterna om kaninen</h1>
+      <CertificateForm />
+    </>
+  );
+}
