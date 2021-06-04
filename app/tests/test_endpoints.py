fe57906d1ab70c236b536263f506ed2db2262833
jonandernovella <novella.uu@gmail.com>
2021-06-04 12:58:30 +0200

    Add test for non-existing individual

--- app/tests/test_endpoints.py
+++ app/tests/test_endpoints.py
@@ -311,6 +311,10 @@ class TestEndpoints(FlaskTest):
             self.assertEqual(download_response.status_code, 200)
             self.assertEqual(download_response.data, first_pdf_response.data)
 
+            # Download a certificate from a non-existing individual
+            download_response = context.get(f"/api/certificates/issue/doesnotexist")
+            self.assertEqual(download_response.status_code, 404)
+
             # Update a certificate
             pdf_response = context.patch(
                 f"/api/certificates/update/{individual}", json=valid_update_form
