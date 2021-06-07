af2378f73b1ee7011f173aa3d446a37f502be30a
Henrike <henrike.wiemker@t-online.de>
2021-06-07 17:29:37 +0200

    make cert form suitable for adding a new rabbit
    introducing a usecase prop as enum
    some styling for the buttons
    let default state for individual be an empty object (might be bad)

--- frontend/src/individual_add.tsx
+++ frontend/src/individual_add.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 
-import { Button } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
 
 import { CertificateForm } from "@app/certificate_form";
 import { Individual } from "@app/data_context_global";
 import { useMessageContext } from "@app/message_context";
 import { post } from "@app/communication";
+import { useUserContext } from "./user_context";
+import { Usecase } from "@app/certificate_form";
+
+const useStyles = makeStyles({
+  paneControls: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: "20px 0",
+  },
+});
 
 export function IndividualAdd() {
-  const [individual, setIndividual] = React.useState(
-    undefined as Individual | undefined
-  );
+  const [individual, setIndividual] = React.useState({} as Individual);
   const { userMessage } = useMessageContext();
+  const { user } = useUserContext();
+  const style = useStyles();
+
+  //returns true if you own the herd the indvidual belongs to, are an admin or the manager of the individual's genebank
+  const canEdit: boolean = React.useMemo(() => {
+    return user?.canEdit(individual?.number);
+  }, [user, individual]);
 
   /**
    * Updates a single field in `individual`.
@@ -27,33 +43,38 @@ export function IndividualAdd() {
   };
 
   const createIndividual = (individual: Individual) => {
-    post("/api/individual", individual).then(
-      () => {
-        console.log("individual sent");
-      },
-      (error) => {
-        console.log(error);
-        userMessage("Något gick fel.", "error");
-      }
-    );
+    if (individual) {
+      post("/api/individual", individual).then(
+        () => {
+          console.log("individual sent");
+        },
+        (error) => {
+          console.log(error);
+          userMessage("Något gick fel.", "error");
+        }
+      );
+    } else {
+      userMessage("Fyll i uppgifterna först", "warning");
+    }
   };
 
   return (
     <>
       <h1>Fyll i uppgifterna om kaninen</h1>
-      <CertificateForm onUpdateIndividual={handleUpdateIndividual} />
-      <div>
+      <CertificateForm
+        onUpdateIndividual={handleUpdateIndividual}
+        individual={individual}
+        canEdit={canEdit}
+        usecase={Usecase.AddIndividual}
+      />
+      <div className={style.paneControls}>
         <Button variant="contained" color="primary">
           Tillbaka
         </Button>
         <Button
           variant="contained"
           color="primary"
-          onClick={
-            individual
-              ? createIndividual(individual)
-              : userMessage("Fyll i uppgifterna först", "warning")
-          }
+          onClick={() => createIndividual(individual)}
         >
           Skapa
         </Button>
