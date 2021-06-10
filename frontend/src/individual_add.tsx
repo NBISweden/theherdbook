e48b9ec795ed88af213bf2a70a4311b6be387f93
Henrike <henrike.wiemker@t-online.de>
2021-06-10 09:55:03 +0200

    move origin genebank into local state

--- frontend/src/individual_add.tsx
+++ frontend/src/individual_add.tsx
@@ -3,7 +3,13 @@ import React from "react";
 import { Button, makeStyles } from "@material-ui/core";
 
 import { CertificateForm } from "@app/certificate_form";
-import { HerdNameID, Individual, LimitedHerd } from "@app/data_context_global";
+import {
+  activeIndividuals,
+  HerdNameID,
+  Individual,
+  LimitedHerd,
+  Genebank,
+} from "@app/data_context_global";
 import { useMessageContext } from "@app/message_context";
 import { post } from "@app/communication";
 import { useUserContext } from "./user_context";
@@ -21,6 +27,9 @@ const useStyles = makeStyles({
 
 export function IndividualAdd({ id }: { id: string }) {
   const [individual, setIndividual] = React.useState({} as Individual);
+  const [genebank, setGenebank] = React.useState(
+    undefined as Genebank | undefined
+  );
   const { userMessage } = useMessageContext();
   const { user } = useUserContext();
   const { genebanks } = useDataContext();
@@ -44,19 +53,24 @@ export function IndividualAdd({ id }: { id: string }) {
     individual && setIndividual({ ...individual, [field]: value });
   };
 
+  React.useEffect(() => {
+    // id here is the id of the herd the user is adding the individual to.
+    // assumes that the new individual originates from a herd belonging to the same genebank.
+    const originGenebank = genebanks.find((genebank) =>
+      genebank.herds.filter((herd) => herd.herd == id)
+    );
+    setGenebank(originGenebank);
+  }, [id]);
+
   const createIndividual = (individual: Individual) => {
     // first generate the individuals origin herd
     if (individual?.number) {
       let newIndividual = individual;
       const numberParts: string[] = individual.number.split("-");
       const originHerdNumber: string = numberParts[0];
-      // id here is the id of the herd the user is adding the individual to.
-      // assumes that the new individual originates from a herd belonging to the same genebank.
-      const originGenebank = genebanks.find((genebank) =>
-        genebank.herds.filter((herd) => herd.herd == id)
-      );
-      const originHerd = originGenebank?.herds.find(
-        (herd) => herd.herd == originHerdNumber
+
+      const originHerd = genebank?.herds.find(
+        (herd: LimitedHerd) => herd.herd == originHerdNumber
       );
 
       if (originHerd?.herd_name && originHerd.herd && originHerd.id) {
@@ -97,6 +111,8 @@ export function IndividualAdd({ id }: { id: string }) {
         canEdit={canEdit}
         usecase={Usecase.AddIndividual}
       />
+      <h2>Lägg till härstamningen</h2>
+      <div></div>
       <div className={style.paneControls}>
         <Button variant="contained" color="primary">
           Tillbaka
