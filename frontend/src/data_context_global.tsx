9d063904e3c36f551e74d42c993f5cf9aec7b66d
Henrike <henrike.wiemker@t-online.de>
2021-08-12 15:10:34 +0200

    fix interfaces for Breeding and ExtendedBreeding

--- frontend/src/data_context_global.tsx
+++ frontend/src/data_context_global.tsx
@@ -113,24 +113,15 @@ export interface Breeding {
   [key: string]: any;
   id?: number;
   breed_date: string | null;
-  breed_notes: string;
+  breed_notes?: string;
   father: string;
   mother: string;
   birth_date: string | null;
-  birth_notes: string;
+  birth_notes?: string;
   litter_size: number | null;
 }
 
-export interface ExtendedBreeding {
-  [key: string]: any;
-  id?: number;
-  breed_date: string | null;
-  breed_notes: string;
-  father: string;
-  mother: string;
-  birth_date: string | null;
-  birth_notes: string;
-  litter_size: number | null;
+export interface ExtendedBreeding extends Breeding {
   mother_name: string | null | undefined;
   father_name: string | null | undefined;
 }
