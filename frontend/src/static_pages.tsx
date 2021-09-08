1e6f671bc7c80d99d8d855a848af066e941d6785
airenzp <airenzp@gmail.com>
2021-09-08 10:42:28 +0200

    restored Typography on footer after fixing css priority

--- frontend/src/static_pages.tsx
+++ frontend/src/static_pages.tsx
@@ -206,12 +206,12 @@ export function Footer() {
       <div className="euLogo">
         <img src="/images/EU-flagga-Europeiska-jordbruksfonden-färg.jpg" />
       </div>
-      <div className="euText">
+      <Typography className="euText">
         Föreningen Gotlandskaninen får stöd från EU för att bevara Gotlands- och
         Mellerudskanin. Stödet går till genbanksbesättningarna,
         medlemstidningen, trycksaker, genbanksintyg, marknadsföring, kurser och
         utbildningar, transport av värdefulla djur samt obduktioner.
-      </div>
+      </Typography>
       <div>
         <img src="/images/logo.png" alt="logo" className="logoImage" />
       </div>
