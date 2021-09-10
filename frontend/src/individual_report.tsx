4970cc92c716fb67fa1379e16d63d005a58ef75f
Henrike <henrike.wiemker@t-online.de>
2021-09-10 15:15:15 +0200

    cleanup individual report

--- frontend/src/individual_report.tsx
+++ frontend/src/individual_report.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import {
   Box,
   Button,
@@ -7,32 +6,20 @@ import {
   FormControl,
   FormControlLabel,
   FormHelperText,
-  FormLabel,
   makeStyles,
-  Radio,
-  RadioGroup,
-  TextField,
   Typography,
 } from "@material-ui/core";
-import {
-  CheckCircle,
-  ExpandMore,
-  ExpandLess,
-  NavigateNext,
-} from "@material-ui/icons";
+import { CheckCircle, NavigateNext } from "@material-ui/icons";
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
-import { Genebank, Individual, inputVariant } from "@app/data_context_global";
-import { useDataContext } from "@app/data_context";
+import { Individual } from "@app/data_context_global";
 import { useMessageContext } from "@app/message_context";
-import { IndividualSellingForm } from "./individual_sellingform";
 import { IndividualSell } from "./individual_sell";
 import { IndividualDeath } from "./individual_death";
 import { patch } from "./communication";
-import { isJsxAttribute } from "typescript";
 
 const useStyles = makeStyles({
   infoText: {
@@ -88,12 +75,6 @@ const useStyles = makeStyles({
     flexDirection: "row",
     alignItems: "center",
   },
-  wideControl: {
-    margin: "5px 0",
-    minWidth: "195px",
-    width: "100%",
-    paddingRight: "5px",
-  },
 });
 
 interface LimitedIndividual {
@@ -103,105 +84,30 @@ interface LimitedIndividual {
 }
 
 export function IndividualReport({ individual }: { individual: Individual }) {
-  const [genebank, setGenebank] = React.useState(
-    undefined as Genebank | undefined
-  );
-  const [individualToReport, setIndividualToReport] = React.useState(
-    individual as Individual
-  );
-  const [showDeathForm, setShowDeathForm] = React.useState(false);
-  const [isDead, setIsDead] = React.useState(false);
   const [reportDate, setReportDate] = React.useState(null as Date | null);
   const [isStillOwner, setIsStillOwner] = React.useState(false);
-  const [checked, setChecked] = React.useState(false as boolean);
-  const [invalidSale, setInvalidSale] = React.useState(false as boolean);
   const [success, setSuccess] = React.useState(false as boolean);
-  const { genebanks } = useDataContext();
+  const [error, setError] = React.useState(false);
   const { userMessage, popup } = useMessageContext();
   const style = useStyles();
-  const disabled: boolean =
-    !individualToReport.herd || !individualToReport.selling_date;
-  const error: boolean = invalidSale && !checked && !disabled;
-
-  React.useEffect(() => {
-    const currentGenebank = genebanks.find((genebank) =>
-      genebank.individuals.some((i) => i.number == individual.number)
-    );
-
-    setGenebank(currentGenebank);
-  }, [individual]);
-
-  /** Check if individual is dead */
-  React.useEffect(() => {
-    if (!!individual.death_date || !!individual.death_note) {
-      setIsDead(true);
-    }
-  }, [individual]);
-
-  /**
-   * Updates a single field in `individualForSale`.
-   *
-   * @param field field name to update
-   * @param value the new value of the field
-   */
-  const handleUpdateIndividual = <T extends keyof Individual>(
-    field: T,
-    value: Individual[T]
-  ) => {
-    individualToReport &&
-      setIndividualToReport({ ...individualToReport, [field]: value });
-    setInvalidSale(false);
-  };
-
-  /**
-   * make sure "butchered", "death notes" and "death_date" always follow isDead
-   * (an alive rabbit can't be butchered=true and can't have death notes or death_date)
-   */
-  React.useEffect(() => {
-    if (!isDead) {
-      handleUpdateIndividual("butchered", false);
-    }
-  }, [isDead]);
-  React.useEffect(() => {
-    if (!isDead && !individualToReport.butchered) {
-      handleUpdateIndividual("death_note", "");
-    }
-  }, [isDead, individualToReport.butchered]);
-  React.useEffect(() => {
-    if (
-      !isDead &&
-      !individualToReport.butchered &&
-      !individualToReport.death_note
-    ) {
-      handleUpdateIndividual("death_date", null);
-    }
-  }, [isDead, individualToReport.death_note, individualToReport.butchered]);
-
-  const reportAsDead = () => {
-    patch("/api/individual", individualToReport).then((json) => {
-      if (json.status == "success") {
-        setSuccess(true);
-        return;
-      }
-      if (json.status == "error") {
-        userMessage("Något gick fel.", "error");
-      }
-    });
-  };
 
   const onSave = () => {
-    if (isDead) {
-      reportAsDead();
-      return;
-    }
     if (!reportDate) {
       userMessage("Ange ett datum för årsrapporten.", "warning");
       return;
     }
+    if (!isStillOwner) {
+      userMessage(
+        "Bekräfta att kaninen finns kvar i din besättning. Finns den inte kvar, välj ett av de andra alternativ.",
+        "warning"
+      );
+      setError(true);
+      return;
+    }
 
     const limitedIndividual: LimitedIndividual = {
-      number: individualToReport.number,
-      herd: individualToReport.herd,
+      number: individual.number,
+      herd: individual.herd,
       yearly_report_date: reportDate,
     };
     patch("/api/individual", limitedIndividual).then((json) => {
@@ -251,23 +157,30 @@ export function IndividualReport({ individual }: { individual: Individual }) {
               />
             </MuiPickersUtilsProvider>
             <FormControl
-              required
-              error={error}
               component="fieldset"
               disabled={!reportDate}
+              error={error}
               className={style.checkContainer}
             >
               <FormControlLabel
                 control={
                   <Checkbox
                     checked={isStillOwner}
-                    onChange={() => setIsStillOwner(!isStillOwner)}
+                    onChange={() => {
+                      if (error) {
+                        setError(false);
+                      }
+                      setIsStillOwner(!isStillOwner);
+                    }}
                     color="primary"
                     inputProps={{ "aria-label": "primary checkbox" }}
                   />
                 }
                 label="Kaninen finns kvar i min besättning på valt datum."
               ></FormControlLabel>
+              <FormHelperText hidden={!error}>
+                Fältet måste vara ifyllt.
+              </FormHelperText>
             </FormControl>
           </div>
           <Typography>
@@ -309,16 +222,10 @@ export function IndividualReport({ individual }: { individual: Individual }) {
             <h2>Klart!</h2>
             <CheckCircle className={style.successIcon} />
           </div>
-          {isDead ? (
-            <p>
-              {individual.name} {individual.number} har rapporterats som död.
-            </p>
-          ) : (
-            <p>
-              Årsrapporten för {individual.name} {individual.number} har lämnats
-              in.
-            </p>
-          )}
+          <p>
+            Årsrapporten för {individual.name} {individual.number} har lämnats
+            in.
+          </p>
         </Box>
       )}
     </div>
