464f8b2054b6625b093b5ed0d08e78fe72209079
airenzp <airenzp@gmail.com>
2022-05-18 13:26:19 +0200

    displaying default herd on selling help

--- frontend/src/individual_sell.tsx
+++ frontend/src/individual_sell.tsx
@@ -85,6 +85,7 @@ export function IndividualSell({ individual }: { individual: Individual }) {
   const disabled: boolean =
     !individualForSale.herd || !individualForSale.selling_date;
   const error: boolean = invalidSale && !checked && !disabled;
+  const defaultHerd = individual.genebank?.charAt(0) + "X1";
 
   React.useEffect(() => {
     const currentGenebank = genebanks.find((genebank) =>
@@ -176,7 +177,7 @@ export function IndividualSell({ individual }: { individual: Individual }) {
           </div>
           <div className={style.formContainer}>
             <IndividualSellingForm
-              herdHelperText="Väl GX1/MX1 om du har sålt kaninen till en person utanför genbanksystemet"
+              herdHelperText={`Väl ${defaultHerd} om du har sålt kaninen till en person utanför genbanksystemet`}
               buyDateHelperText="Vänligen ange datum för försäljningen"
               individual={individualForSale}
               herdOptions={genebank ? genebank.herds : []}
