34555e9e847f7d79caef1ce137b147da6e8e6296
Jonas Hagberg <jonas.hagberg@nbis.se>
2022-12-10 20:46:42 +0100

    Major rewrite of number suggestion now handle lot more cases

--- app/utils/data_access.py
+++ app/utils/data_access.py
@@ -914,11 +914,20 @@ def add_individual(form, user_uuid):
         return {"status": "error", "message": "Forbidden"}
 
     if form.get("number", None) is None and "breeding" in form:
-        form["number"] = next_individual_number(
+        nextind = next_individual_number(
             herd=form["herd"],
             birth_date=form["birth_date"],
             breeding_event=form["breeding"],
         )
+        logger.error(f"nextind is ${nextind}")
+        if nextind.get("status", None) == "success":
+            form["number"] = nextind["number"]
+        else:
+            logger.error(f"Next in is not successfull: ${nextind.get('message')}")
+            return {
+                "status": "error",
+                "message": f"kan inte hämta nästa individ nummer: ${nextind.get('message')}",
+            }
 
     if Individual.select().where(Individual.number == form["number"]).exists():
         return {"status": "error", "message": "Individual number already exists"}
